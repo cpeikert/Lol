db55e716070a977490f28b3acc2e94f321554d18
Eric <ecrockett0@gmail.com>
2016-01-23 01:29:34 -0500

    Added more simple and sanity checks

--- benchmarks/CycBenches.hs
+++ benchmarks/CycBenches.hs
@@ -15,7 +15,11 @@ import Utils
 cycBenches :: (MonadRandom rnd) => rnd Benchmark
 cycBenches = bgroupRnd "Cyc"
   [bgroupRnd "CRT + *" $ groupC $ wrap2Arg bench_mulPow,
-   bgroupRnd "*" $ groupC $ wrap2Arg bench_mul]
+   bgroupRnd "*" $ groupC $ wrap2Arg bench_mul,
+   bgroupRnd "id1" $ groupC $ wrap1Arg bench_adviseCRT,
+   bgroupRnd "id2" $ groupC $ wrap1Arg bench_advisePowPow,
+   bgroupRnd "crt" $ groupC $ wrap1Arg bench_advisePowCRT,
+   bgroupRnd "crtInv" $ groupC $ wrap1Arg bench_advisePow]
 
 bench_mulPow :: (CElt t r, Fact m) => Cyc t m r -> Cyc t m r -> Benchmarkable
 bench_mulPow a b = 
@@ -26,7 +30,24 @@ bench_mulPow a b =
 bench_mul :: (CElt t r, Fact m) => Cyc t m r -> Cyc t m r -> Benchmarkable
 bench_mul a b = nf (a *) b
 
+bench_adviseCRT :: (CElt t r, Fact m) => Cyc t m r -> Benchmarkable
+bench_adviseCRT = nf adviseCRT
+
+bench_advisePow :: (CElt t r, Fact m) => Cyc t m r -> Benchmarkable
+bench_advisePow = nf advisePow
+
+bench_advisePowCRT :: (CElt t r, Fact m) => Cyc t m r -> Benchmarkable
+bench_advisePowCRT x = let y = advisePow x in nf adviseCRT y
+
+
+bench_advisePowPow :: (CElt t r, Fact m) => Cyc t m r -> Benchmarkable
+bench_advisePowPow x = let y = advisePow x in nf advisePow y
+
 type BasicCtx t m r = (CElt t r, Fact m)
+
+wrap1Arg :: (BasicCtx t m r, MonadRandom rnd) 
+  => (Cyc t m r -> Benchmarkable) -> Proxy t -> Proxy '(m,r) -> String -> rnd Benchmark
+wrap1Arg f _ _ str = (bench str) <$> (genArgs f)
 
 wrap2Arg :: (BasicCtx t m r, MonadRandom rnd) 
   => (Cyc t m r -> Cyc t m r -> Benchmarkable) -> Proxy t -> Proxy '(m,r) -> String -> rnd Benchmark
