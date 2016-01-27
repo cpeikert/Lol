851b82322bc49395c7243e3829256df47a852d09
Eric <ecrockett0@gmail.com>
2016-01-27 13:10:45 -0500

    added more benchmarks to SHE

--- benchmarks/SHEBenches.hs
+++ benchmarks/SHEBenches.hs
@@ -30,10 +30,81 @@ bench_gensk v _ _ _ = nfIO $ do
   gen <- newGenIO -- uses system entropy
   return $ evalRand (genSK v :: Rand (CryptoRand gen) (SK (Cyc t m Int64))) gen
 
+bench_enc :: forall t m m' z zp zq gen . (EncryptCtx t m m' z zp zq, CryptoRandomGen gen)
+  => Proxy gen -> Proxy zq -> SK (Cyc t m' z) -> PT (Cyc t m zp) -> Benchmarkable
+bench_enc _ _ sk pt = nfIO $ do
+  gen <- newGenIO
+  return $ evalRand (encrypt sk pt :: Rand (CryptoRand gen) (CT m zp (Cyc t m' zq))) gen
+
+bench_dec :: (DecryptCtx t m m' z zp zq) => SK (Cyc t m' z) -> CT m zp (Cyc t m' zq) -> Benchmarkable
+bench_dec sk ct = nf (decrypt sk) ct
+
+bench_mul :: (Ring (CT m zp (Cyc t m' zq)), NFData (CT m zp (Cyc t m' zq)))
+  => CT m zp (Cyc t m' zq) -> CT m zp (Cyc t m' zq) -> Benchmarkable
+bench_mul a = nf (*a)
+
+bench_keySwQ :: KSHint m zp t m' zq' gad zq'' -> CT m zp (Cyc t m' zq') -> Benchmarkable
+bench_keySwQ (KeySwitch kswq) x = nf kswq $ x*x
+
+bench_rescaleCT :: (RescaleCyc (Cyc t) zq' zq, NFData (CT m zp (Cyc t m' zq))) 
+  => Proxy zq -> CT m zp (Cyc t m' zq') -> Benchmarkable
+bench_rescaleCT _ = nf rescaleLinearCT
+
+newtype KSHint m zp t m' zq gad zq' = KeySwitch (CT m zp (Cyc t m' zq) -> CT m zp (Cyc t m' zq))
+
+-- sanity check: should take as long as the sum of its component tests
+bench_mulCycle :: forall gad z zp zq zq' zq'' t m m' . (
+                   Ring (CT m zp (Cyc t m' zq')), NFData (CT m zp (Cyc t m' zq)),
+                   RescaleCyc (Cyc t) zq' zq, ToSDCtx t m' zp zq', z~ModRep zp,
+                   KeySwitchCtx gad t m' zp zq' zq'') 
+  => Proxy zq
+     -> KSHint m zp t m' zq' gad zq''
+     -> CT m zp (Cyc t m' zq')
+     -> CT m zp (Cyc t m' zq')
+     -> Benchmarkable
+bench_mulCycle _ (KeySwitch kswq) a b = 
+  nf (rescaleLinearCT . kswq . (a*) :: CT m zp (Cyc t m' zq') -> CT m zp (Cyc t m' zq)) b
+
+
+
+
+
+
+
+
+
+
+
+
+
 wrapGenSK :: forall t m gen rnd . (GenSKCtx t m Int64 Double, CryptoRandomGen gen, Monad rnd)
   => (Proxy gen -> Proxy t -> Proxy m -> Benchmarkable) 
      -> Proxy gen -> Proxy t -> Proxy m -> rnd Benchmark
 wrapGenSK f _ _ _ = return $ bench (show (BT :: BenchType m)) $ f Proxy Proxy Proxy
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 groupGens :: (Monad rnd) => 
   (forall t m gen .
@@ -71,34 +142,7 @@ groupMRLift f =
 
 
 
-bench_enc :: forall t m m' z zp zq gen . (EncryptCtx t m m' z zp zq, CryptoRandomGen gen)
-  => Proxy gen -> Proxy zq -> SK (Cyc t m' z) -> PT (Cyc t m zp) -> Benchmarkable
-bench_enc _ _ sk pt = nfIO $ do
-  gen <- newGenIO
-  return $ evalRand (encrypt sk pt :: Rand (CryptoRand gen) (CT m zp (Cyc t m' zq))) gen
 
-bench_dec :: (DecryptCtx t m m' z zp zq) => SK (Cyc t m' z) -> CT m zp (Cyc t m' zq) -> Benchmarkable
-bench_dec sk ct = nf (decrypt sk) ct
-
-bench_mul :: (Ring (CT m zp (Cyc t m' zq)), NFData (CT m zp (Cyc t m' zq)))
-  => CT m zp (Cyc t m' zq) -> CT m zp (Cyc t m' zq) -> Benchmarkable
-bench_mul a = nf (*a)
-
-
-newtype KSHint m zp t m' zq gad zq' = KeySwitch (CT m zp (Cyc t m' zq) -> CT m zp (Cyc t m' zq))
-
--- EAC: split up
-bench_mulCycle :: forall gad z zp zq zq' zq'' t m m' . (
-                   Ring (CT m zp (Cyc t m' zq')), NFData (CT m zp (Cyc t m' zq)),
-                   RescaleCyc (Cyc t) zq' zq, ToSDCtx t m' zp zq', z~ModRep zp,
-                   KeySwitchCtx gad t m' zp zq' zq'', KSHintCtx gad t m' z zq'') 
-  => Proxy zq
-     -> KSHint m zp t m' zq' gad zq''
-     -> CT m zp (Cyc t m' zq') 
-     -> CT m zp (Cyc t m' zq') 
-     -> Benchmarkable
-bench_mulCycle _ (KeySwitch kswq) a b = 
-  nf (rescaleLinearCT . kswq . (a*) :: CT m zp (Cyc t m' zq') -> CT m zp (Cyc t m' zq)) b
 
 
 
