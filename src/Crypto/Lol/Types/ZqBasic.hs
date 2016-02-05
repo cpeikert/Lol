76eb3efd75b6f2ea15c4fd4cde8696c883228f8c
Chris Peikert <cpeikert@alum.mit.edu>
2016-02-05 17:22:16 -0500

    cleaned up ZqBasic Unbox-related instances

--- src/Crypto/Lol/Types/ZqBasic.hs
+++ src/Crypto/Lol/Types/ZqBasic.hs
@@ -317,30 +317,31 @@ newtype instance U.MVector s (ZqBasic q z) = MV_ZqBasic (U.MVector s z)
 newtype instance U.Vector (ZqBasic q z) = V_ZqBasic (U.Vector z)
 
 -- Unbox, when underlying representation is
-instance (U.Unbox z) => U.Unbox (ZqBasic q z)
+instance U.Unbox z => U.Unbox (ZqBasic q z)
 
 {- purloined and tweaked from code in `vector` package that defines
 types as unboxed -}
-instance (U.Unbox z) => M.MVector U.MVector (ZqBasic q z) where
+instance U.Unbox z => M.MVector U.MVector (ZqBasic q z) where
   basicLength (MV_ZqBasic v) = M.basicLength v
   basicUnsafeSlice z n (MV_ZqBasic v) = MV_ZqBasic $ M.basicUnsafeSlice z n v
   basicOverlaps (MV_ZqBasic v1) (MV_ZqBasic v2) = M.basicOverlaps v1 v2
   basicInitialize (MV_ZqBasic v) = M.basicInitialize v
-  basicUnsafeNew n = MV_ZqBasic `liftM` M.basicUnsafeNew n
-  basicUnsafeReplicate n (ZqB x) = MV_ZqBasic `liftM` M.basicUnsafeReplicate n x
-  basicUnsafeRead (MV_ZqBasic v) z = ZqB `liftM` M.basicUnsafeRead v z
+  basicUnsafeNew n = MV_ZqBasic <$> M.basicUnsafeNew n
+  basicUnsafeReplicate n (ZqB x) = MV_ZqBasic <$> M.basicUnsafeReplicate n x
+  basicUnsafeRead (MV_ZqBasic v) z = ZqB <$> M.basicUnsafeRead v z
   basicUnsafeWrite (MV_ZqBasic v) z (ZqB x) = M.basicUnsafeWrite v z x
   basicClear (MV_ZqBasic v) = M.basicClear v
   basicSet (MV_ZqBasic v) (ZqB x) = M.basicSet v x
   basicUnsafeCopy (MV_ZqBasic v1) (MV_ZqBasic v2) = M.basicUnsafeCopy v1 v2
   basicUnsafeMove (MV_ZqBasic v1) (MV_ZqBasic v2) = M.basicUnsafeMove v1 v2
-  basicUnsafeGrow (MV_ZqBasic v) n = MV_ZqBasic `liftM` M.basicUnsafeGrow v n
+  basicUnsafeGrow (MV_ZqBasic v) n = MV_ZqBasic <$> M.basicUnsafeGrow v n
 
-instance (U.Unbox z) => G.Vector U.Vector (ZqBasic q z) where
-  basicUnsafeFreeze (MV_ZqBasic v) = V_ZqBasic `liftM` G.basicUnsafeFreeze v
-  basicUnsafeThaw (V_ZqBasic v) = MV_ZqBasic `liftM` G.basicUnsafeThaw v
+instance U.Unbox z => G.Vector U.Vector (ZqBasic q z) where
+  basicUnsafeFreeze (MV_ZqBasic v) = V_ZqBasic <$> G.basicUnsafeFreeze v
+  basicUnsafeThaw (V_ZqBasic v) = MV_ZqBasic <$> G.basicUnsafeThaw v
   basicLength (V_ZqBasic v) = G.basicLength v
   basicUnsafeSlice z n (V_ZqBasic v) = V_ZqBasic $ G.basicUnsafeSlice z n v
-  basicUnsafeIndexM (V_ZqBasic v) z = ZqB `liftM` G.basicUnsafeIndexM v z
+  basicUnsafeIndexM (V_ZqBasic v) z = ZqB <$> G.basicUnsafeIndexM v z
   basicUnsafeCopy (MV_ZqBasic mv) (V_ZqBasic v) = G.basicUnsafeCopy mv v
   elemseq _ = seq
+  
