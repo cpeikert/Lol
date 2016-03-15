c0c9d392a989b413eba31eb57bc5d12933ce3d5e
Eric <ecrockett0@gmail.com>
2016-03-14 21:01:17 -0400

    Init commit

--- challenges/Main.hs
+++ challenges/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE BangPatterns, DataKinds, NoImplicitPrelude, PackageImports, RebindableSyntax, ScopedTypeVariables, TemplateHaskell #-}
+{-# LANGUAGE DataKinds, NoImplicitPrelude, PackageImports, RebindableSyntax, ScopedTypeVariables #-}
 
 import TGaussian
 import LWE
@@ -9,59 +9,85 @@ import Utils
 
 import Data.ByteString as BS hiding (init, tail, map)
 import Data.Serialize
-import Crypto.Lol hiding (encode, errorRounded)
+import Crypto.Lol hiding (encode)
 import System.IO as IO
 import Algebra.IntegralDomain (divUp)
 
 import "crypto-api" Crypto.Random
 import Crypto.Random.DRBG
 import Control.Applicative
-import Data.BooleanList (byteStringToBooleanList)
+import Data.BooleanList (byteStringToBooleanList) -- boolean-list
 import Data.ByteString.Builder
 import Numeric (readHex)
 import Data.Char
 import Net.Beacon
 import Control.Monad.Random
-import Crypto.Lol.GaussRandom
-
-import qualified Data.Vector.Unboxed as V -- (toList)
-import Control.Monad
-
-
-type Dim = $(fType $ 128)
-type FPrec = BigFloat (Prec 50)
-type T = RT
-
-fastSampleSum :: forall v m q . (ToRational v, OrdFloat q, Random q, MonadRandom m) => v -> Int -> m q
-fastSampleSum v num = go num 0
-  where go :: Int -> q -> m q
-        go 0 !acc = return acc
-        go x !acc = do
-          (a,b) <- realGaussian v
-          go (x-2) $ acc + a*a + b*b
 
 main :: IO ()
 main = do
+  -- Generate (numInstances * fsInstSize) LWE instances,
+  -- corresponding to (numInstances * fsInstSize * numSamples) LWE samples.
+  -- We reveal all but numInstances LWE keys.
+  let --numInstances = 1   -- number of secret LWE instances to generate
+      numSamples   = 1  -- number of LWE samples per instance
+      --fsInstSize   = 2   -- number of LWE instances per FS instance (we reveal keys for all but one)
+      v = 1
 
-  let num = 10000000
-      v = 1 :: FPrec
-  --samples <- V.map (^(2::Int)) <$> realGaussians v num
-  --print $ "sample average: " ++ (show $ (fromIntegral num) / (V.sum samples :: FPrec))
-  sum' :: FPrec <- fastSampleSum v num
-  print $ (fromIntegral num) / sum'
+  -- check LWE instance
+  inst :: LWEInstance Double RT F11 (Zq 23) <- proxyT (lweInstance v numSamples) (Proxy::Proxy (BigFloat (Prec 25)))
+  if checkInstance inst
+  then print "Instance passed."
+  else print "Instance failed."
 
-  let n = proxy totientFact (Proxy::Proxy Dim)
-      mhat = proxy valueHatFact (Proxy::Proxy Dim)
-      bound = (fromIntegral $ mhat*n)*v
-      numErrs = 10000
+  -- check instance serialization
+  BS.writeFile "lwe.raw" (encode inst)
+  bsInst <- decode' <$> BS.readFile "lwe.raw"
+  if (bsInst == inst)
+  then print "Serialization passed."
+  else print "Serialization failed."
 
-  realErrs :: [Cyc T Dim FPrec] <- replicateM numErrs $ tGaussian v
-  rndErrs :: [Cyc T Dim Int64] <- replicateM numErrs $ proxyT (errorRounded v) (Proxy::Proxy FPrec)
-  let realNorms = map gSqNorm realErrs
-      rndNorms = map gSqNorm rndErrs
+  -- check instance printing/parsing
+  IO.writeFile "lwe.txt" (show inst)
+  txInst <- read <$> IO.readFile "lwe.txt"
+  if(txInst == inst)
+  then print "Text parse passed."
+  else print "Text parse failed."
 
-      realRatios = map ((bound) /) realNorms
-      rndRatios = map (((bound) /) . fromIntegral) rndNorms
+  -- get Beacon
+  time <- localDateToSeconds 2 24 2016 11 0
+  seed <- getBeacon time
+  print $ showHexBS seed
 
-  print $ "Continuous noise to bound ratio: " ++ (show $ average realRatios)
-  print $ "Rounded noise to bound ratio: " ++ (show $ average rndRatios)
+  -- get random bits using Beacon as seed to generator
+  let (Right gen) = newGen seed
+      bits = evalRand (genBits 8) (gen :: HashDRBG)
+  print bits
+
+
+
+
+
+
+
+
+
+
+
+decode' :: (Serialize a) => ByteString -> a
+decode' bs = 
+  case decode bs of
+    (Left str) -> error $ "when decoding bytestring. Got string " ++ str
+    (Right x) -> x
+
+showHexBS :: ByteString -> String
+showHexBS = map toUpper . tail . init . show . toLazyByteString . byteStringHex
+
+-- outputs the requested number of bits
+-- wastes any unused bits in the last byte sampled
+genBits :: (CryptoRandomGen gen) => Int -> Rand gen [Bool]
+genBits n = liftRand $ \g ->
+  let numBytes = n `divUp` 8
+      mbytes = genBytes numBytes g
+  in case mbytes of
+      (Left err) -> error "An error occured when sampling bits."
+      (Right (bs,g')) -> (byteStringToBooleanList bs, g')
