be321a6de2771690867bb98c4800da8a4bd23723
Eric <ecrockett0@gmail.com>
2016-03-29 19:08:49 -0400

    moved intLog

--- lol/Crypto/Lol/Types/Random.hs
+++ lol/Crypto/Lol/Types/Random.hs
@@ -18,6 +18,12 @@ intBytes =
   let bits = intLog 2 $ (1 + (fromIntegral (maxBound :: Int)) - (fromIntegral (minBound :: Int)) :: Integer)
   in if (bits `mod` 8) == 0 then bits `div` 8 else error "invalid Int bits in `intBytes`"
 
+bytesToInt :: ByteString -> Int
+bytesToInt bs = case intBytes of
+  4 -> fromIntegral $ runGet getWord32host $ pack $ unpack bs
+  8 -> fromIntegral $ runGet getWord64host $ pack $ unpack bs
+  _ -> error "Unsupported Int size in `bytesToInt`"
+
 -- | Yield @log_b(n)@ when it is a non-negative integer (otherwise
 -- error).
 intLog :: (Integral i) => i -> i -> Int
@@ -25,12 +31,6 @@ intLog _ 1 = 0
 -- correct because ceil (lg (x)) == ceil (log (ceil (x)))
 intLog b n | (n `mod` b) == 0 = 1 + intLog b (n `div` b)
            | otherwise = error "invalid arguments to intLog"
-
-bytesToInt :: ByteString -> Int
-bytesToInt bs = case intBytes of
-  4 -> fromIntegral $ runGet getWord32host $ pack $ unpack bs
-  8 -> fromIntegral $ runGet getWord64host $ pack $ unpack bs
-  _ -> error "Unsupported Int size in `bytesToInt`"
 
 instance (CryptoRandomGen g) => RandomGen (CryptoRand g) where
   next (CryptoRand g) = case genBytes intBytes g of
