510d122c287f36c95724edffce42382efdb14829
Chris Peikert <cpeikert@alum.mit.edu>
2016-04-18 16:06:36 -0400

    stylish and hlint

--- lol/Crypto/Lol/Types/Proto.hs
+++ lol/Crypto/Lol/Types/Proto.hs
@@ -1,14 +1,12 @@
-{-# LANGUAGE FlexibleContexts, FlexibleInstances, PolyKinds, ScopedTypeVariables, TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts, FlexibleInstances, PolyKinds,
+             ScopedTypeVariables, TypeFamilies #-}
+
 module Crypto.Lol.Types.Proto where
 
-import Crypto.Lol.LatticePrelude (proxy, Proxy(..), lift, reduce, map)
-
 import Data.ByteString.Lazy hiding (map)
-import Data.Foldable (toList)
-import Data.Sequence as S (fromList)
 
+import Text.ProtocolBuffers        (messageGet, messagePut)
 import Text.ProtocolBuffers.Header
-import Text.ProtocolBuffers (messageGet, messagePut)
 
 class Protoable a where
   type ProtoType a
@@ -16,10 +14,12 @@ class Protoable a where
   toProto :: a -> ProtoType a
   fromProto :: ProtoType a -> a
 
-msgPut :: (ReflectDescriptor (ProtoType a), Wire (ProtoType a), Protoable a) => a -> ByteString
+msgPut :: (ReflectDescriptor (ProtoType a), Wire (ProtoType a), Protoable a)
+          => a -> ByteString
 msgPut = messagePut . toProto
 
-msgGet :: (ReflectDescriptor (ProtoType a), Wire (ProtoType a), Protoable a) => ByteString -> Either String (a, ByteString)
+msgGet :: (ReflectDescriptor (ProtoType a), Wire (ProtoType a), Protoable a)
+          => ByteString -> Either String (a, ByteString)
 msgGet bs = do
   (msg, bs') <- messageGet bs
   return (fromProto msg, bs')
