a40f156917801cded689e4f7f9d51608a0fd3c92
Chris Peikert <cpeikert@alum.mit.edu>
2016-04-18 14:07:27 -0400

    module re-export trick

--- lol/Crypto/Lol.hs
+++ lol/Crypto/Lol.hs
@@ -2,22 +2,15 @@
 -- | Re-exports primary interfaces.
 
 module Crypto.Lol 
-( module Crypto.Lol.Cyclotomic.Cyc
-, module Crypto.Lol.Gadget
-, module Crypto.Lol.LatticePrelude
+( module X
+) where
 
-, module Crypto.Lol.Types.RRq
-, module Crypto.Lol.Types.ZqBasic
-, module Crypto.Lol.Cyclotomic.Tensor.CTensor
-, module Crypto.Lol.Cyclotomic.Tensor.RepaTensor
-, module Crypto.Lol.Types.IrreducibleChar2) where
+import Crypto.Lol.Cyclotomic.Cyc as X
+import Crypto.Lol.Gadget as X
+import Crypto.Lol.LatticePrelude as X
 
-import Crypto.Lol.Cyclotomic.Cyc
-import Crypto.Lol.Gadget
-import Crypto.Lol.LatticePrelude
-
-import Crypto.Lol.Types.RRq
-import Crypto.Lol.Types.ZqBasic
-import Crypto.Lol.Cyclotomic.Tensor.CTensor
-import Crypto.Lol.Cyclotomic.Tensor.RepaTensor
-import Crypto.Lol.Types.IrreducibleChar2
+import Crypto.Lol.Types.RRq as X
+import Crypto.Lol.Types.ZqBasic as X
+import Crypto.Lol.Cyclotomic.Tensor.CTensor as X
+import Crypto.Lol.Cyclotomic.Tensor.RepaTensor as X
+import Crypto.Lol.Types.IrreducibleChar2 as X
