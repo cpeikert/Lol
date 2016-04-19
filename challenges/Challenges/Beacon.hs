eaa0c33bfd762ae531694cd9b5578d0aede25670
Chris Peikert <cpeikert@alum.mit.edu>
2016-04-19 10:24:02 -0400

    Cleanups

--- challenges/Challenges/Beacon.hs
+++ challenges/Challenges/Beacon.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 -- | Utility functions for working with the
 -- <http://www.nist.gov/itl/csd/ct/nist_beacon.cfm NIST Randomness
 -- Beacon>.
@@ -19,64 +21,39 @@ import Data.Time.LocalTime   (getCurrentTimeZone, timeZoneMinutes)
 
 -- | The number of bytes (@512 / 8 = 64@) per beacon.
 bytesPerBeacon :: Int
-bytesPerBeacon = 64 :: Int
+bytesPerBeacon = 64
 
 -- | The number of seconds (@60@) between beacon outputs.
 beaconInterval :: Int
-beaconInterval = 60 :: Int
+beaconInterval = 60
 
 -- | Represents a byte offset in a beacon output at a particular time.
 data BeaconPos = BP Int Int deriving (Eq, Show)
 instance NFData BeaconPos where rnf (BP a b) = rnf a `seq` rnf b
 
--- | Advances the beacon position by one byte, overflowing to the next beacon in necessary.
-advanceBeaconPos :: (Monad m) => StateT BeaconPos m ()
+-- | Advances the beacon position by one byte, overflowing to the next
+-- beacon in necessary.
+advanceBeaconPos :: (MonadState BeaconPos m) => m ()
 advanceBeaconPos = do
   (BP time byteOffset) <- get
   if byteOffset == bytesPerBeacon
   then put (BP (time+beaconInterval) 0)
   else put (BP time (byteOffset+1))
 
--- | The number of seconds elapsed from a given GMT time since the (GMT) epoch.
+-- | The number of seconds elapsed from a given GMT time since the
+-- (GMT) epoch.
 gmtDateToSeconds :: Int -> Int -> Integer -> Int -> Int -> Int
-gmtDateToSeconds month day year hour minute | hour >= 0 &&
-                                           hour < 24 &&
-                                           minute >= 0 &&
-                                           minute < 60 =
-  round $ utcTimeToPOSIXSeconds $ UTCTime (fromGregorian year month day) (secondsToDiffTime $ fromIntegral $ 3600*hour+60*minute)
+gmtDateToSeconds month day year hour minute |
+  hour >= 0 && hour < 24 && minute >= 0 && minute < 60 =
+    round $ utcTimeToPOSIXSeconds $
+    UTCTime (fromGregorian year month day)
+    (secondsToDiffTime $ fromIntegral $ 3600*hour+60*minute)
 gmtDateToSeconds _ _ _ _ _ = error "invalid date to gmtDateToSeconds"
 
--- | The number of seconds elapsed from a given local time since the (GMT) epoch.
+-- | The number of seconds elapsed from a given local time since the
+-- (GMT) epoch.
 localDateToSeconds :: Int -> Int -> Integer -> Int -> Int -> IO Int
 localDateToSeconds month day year hour minute = do
   let gmt = gmtDateToSeconds month day year hour minute
   minuteOffset <- timeZoneMinutes <$> getCurrentTimeZone
   return $ gmt - (minuteOffset*60)
-
--- http://hackaday.com/2014/12/19/nist-randomness-beacon/
-{-
-verifySig :: X509 -> Record -> IO Bool
-verifySig cert rec = do
-  pk <- getPublicKey cert
-  dgst <- fromJust' "Digest alg" <$> getDigestByName "sha512"
-  -- the signature is stored in little-endian
-  let sig = signatureValue rec
-      revSig = B.toStrict $ B.pack $ reverse $ B.unpack sig
-      msg = B.toStrict $ signedMsg rec
-  vs <- verifyBS dgst revSig pk msg
-  let vs' = case vs of
-              VerifySuccess -> True
-              VerifyFailure -> False
-      expectedOutput = hash $ B.toStrict sig
-      actualOutput = B.toStrict $ outputValue rec
-  return $ vs' && (expectedOutput == actualOutput)
-
-signedMsg :: Record -> B.ByteString
-signedMsg rec = B.concat [
-  SB.pack $ version rec,
-  B.toLazyByteString $ B.word32BE $ fromIntegral $ frequency rec,
-  B.toLazyByteString $ B.word64BE $ fromIntegral $ timeStamp rec,
-  seedValue rec,
-  previousOutputValue rec,
-  B.toLazyByteString $ B.word32BE $ fromIntegral $ statusCode rec]
--}
