2eaa85fdd5280ba79574cdde554467eef864daca
Chris Peikert <cpeikert@alum.mit.edu>
2016-04-19 14:26:00 -0400

    better types and instances

--- challenges/Challenges/ContinuousLWE/Proto.hs
+++ challenges/Challenges/ContinuousLWE/Proto.hs
@@ -1,74 +1,90 @@
-{-# LANGUAGE FlexibleInstances, GADTs, ScopedTypeVariables,
-             StandaloneDeriving, TypeFamilies, UndecidableInstances #-}
+{-# LANGUAGE FlexibleContexts, FlexibleInstances, GADTs,
+             ScopedTypeVariables, StandaloneDeriving, TypeFamilies,
+             UndecidableInstances #-}
 
--- | Translates unparameterized ProtoTypes into parameterized Haskell types.
+-- | Translates unparameterized prototypes into parameterized Haskell
+-- types.
 
 module Challenges.ContinuousLWE.Proto
-( ContLWEInstance(..)
-, ContLWESample(..)
-, LWESecret(..)
+( RLWEInstanceCont(..)
+, RLWESampleCont
+, RLWESecret
 ) where
 
 import Control.DeepSeq
 
-import Crypto.Lol (Cyc, proxy, Proxy(..), valueFact, Int64, modulus, Mod(..), Fact, Factored)
-import Crypto.Lol.Cyclotomic.UCyc
-import Crypto.Lol.Types.Proto
-import Crypto.Lol.Types.Proto.R
-import Crypto.Lol.Types.Proto.Rq
-import Crypto.Lol.Types.Proto.Kq
-import qualified Challenges.Proto.ContLWEInstance as P
-import qualified Challenges.Proto.ContLWESample as P
-import qualified Challenges.Proto.LWESecret as P
+import qualified Challenges.Proto.RLWE.RLWEInstanceCont as P
+import qualified Challenges.Proto.RLWE.RLWESampleCont   as P
+import qualified Challenges.Proto.RLWE.RLWESecret       as P
+import           Crypto.Lol
+import           Crypto.Lol.Cyclotomic.UCyc
+import           Crypto.Lol.Types.Proto
+import           Crypto.Lol.Types.Proto.Kq
+import           Crypto.Lol.Types.Proto.R
+import           Crypto.Lol.Types.Proto.Rq
 
 import Data.Foldable as S (toList)
-import Data.Reflection hiding (D)
-import Data.Sequence as S (fromList, Seq)
+import Data.Sequence as S (fromList)
+import Data.Word
 
--- | Corresponds to LWESecret proto type.
-data LWESecret t m z = LWESecret Int (Cyc t m z)
-deriving instance (Show (Cyc t m z)) => Show (LWESecret t m z)
-deriving instance (Eq (Cyc t m z)) => Eq (LWESecret t m z)
-instance (NFData (Cyc t m z)) => NFData (LWESecret t m z) where
-  rnf (LWESecret idx s) = rnf idx `seq` rnf s
-instance (Protoable (Cyc t m z), ProtoType (Cyc t m z) ~ R, Fact m) => Protoable (LWESecret t m z) where
-  type ProtoType (LWESecret t m z) = P.LWESecret
-  toProto (LWESecret idx s) = 
-    P.LWESecret (fromIntegral idx) (fromIntegral $ proxy valueFact (Proxy::Proxy m)) $ toProto s
-  fromProto (P.LWESecret idx m s) = 
-    LWESecret (fromIntegral idx) $ fromProto s
+-- | Corresponds to 'RLWESecret' proto type.
+type RLWESecret t m z = (Word32, Cyc t m z)
 
--- | Corresponds to ContLWEInstance proto type.
-data ContLWEInstance v t m zq rq = ContLWEInstance Int v v [ContLWESample t m zq rq]
-instance (NFData (ContLWESample t m zq rq), NFData v) => NFData (ContLWEInstance v t m zq rq) where
-  rnf (ContLWEInstance idx v bound ss) = (rnf idx) `seq` (rnf v) `seq` (rnf bound) `seq` (rnf ss)
-deriving instance (Show (ContLWESample t m zq rq), Show v) => Show (ContLWEInstance v t m zq rq)
-deriving instance (Eq (ContLWESample t m zq rq), Eq v) => Eq (ContLWEInstance v t m zq rq)
-instance (Protoable (ContLWESample t m zq rq), Mod zq, ModRep zq ~ Int64, Fact m)
-  => Protoable (ContLWEInstance Double t m zq rq) where
-  type ProtoType (ContLWEInstance Double t m zq rq) = P.ContLWEInstance
-  toProto (ContLWEInstance idx v bound samples) = 
-    P.ContLWEInstance
-      (fromIntegral idx) 
-      (fromIntegral (proxy valueFact (Proxy::Proxy m)))
-      (fromIntegral (proxy modulus (Proxy::Proxy zq)))
-      v
-      bound
-      (S.fromList $ map toProto samples)
-  fromProto (P.ContLWEInstance idx m q v bound samples) = 
-    ContLWEInstance (fromIntegral idx) v bound $ map fromProto $ S.toList samples
+-- | Corresponds to 'RLWESampleCont' proto type.
+type RLWESampleCont t m zq rq = (Cyc t m zq, UCyc t m D rq)
 
--- | Corresponds to ContLWESample proto type.
-data ContLWESample t m zq rq = ContLWESample (Cyc t m zq) (UCyc t m D rq)
-deriving instance (Show (Cyc t m zq), Show (UCyc t m D rq)) => Show (ContLWESample t m zq rq)
-deriving instance (Eq (Cyc t m zq), Eq (UCyc t m D rq)) => Eq (ContLWESample t m zq rq)
-instance (NFData (Cyc t m zq), NFData (UCyc t m D rq)) => NFData (ContLWESample t m zq rq) where
-  rnf (ContLWESample a b) = (rnf a) `seq` (rnf b)
-instance (Protoable (Cyc t m zq), 
-          ProtoType (Cyc t m zq) ~ Rq,
-          Protoable (UCyc t m D rq),
-          ProtoType (UCyc t m D rq) ~ Kq) 
-  => Protoable (ContLWESample t m zq rq) where
-  type ProtoType (ContLWESample t m zq rq) = P.ContLWESample
-  toProto (ContLWESample a b) = P.ContLWESample (toProto a) (toProto b)
-  fromProto (P.ContLWESample a b) = ContLWESample (fromProto a) (fromProto b)
+-- | Corresponds to 'RLWEInstanceCont' proto type.
+data RLWEInstanceCont v t m zq rq =
+  RLWEInstanceCont Word32 v v [RLWESampleCont t m zq rq]
+
+deriving instance (Show (RLWESampleCont t m zq rq), Show v)
+                  => Show (RLWEInstanceCont v t m zq rq)
+deriving instance (Eq (RLWESampleCont t m zq rq), Eq v)
+                  => Eq (RLWEInstanceCont v t m zq rq)
+
+instance (NFData (RLWESampleCont t m zq rq), NFData v)
+         => NFData (RLWEInstanceCont v t m zq rq) where
+  rnf (RLWEInstanceCont idx v bound ss) =
+    rnf idx `seq` rnf v `seq` rnf bound `seq` rnf ss
+
+instance (Protoable (Cyc t m z), ProtoType (Cyc t m z) ~ R, Fact m)
+         => Protoable (RLWESecret t m z) where
+  type ProtoType (RLWESecret t m z) = P.RLWESecret
+
+  toProto (idx,s) = P.RLWESecret idx
+    (fromIntegral $ proxy valueFact (Proxy::Proxy m)) $ toProto s
+
+  fromProto = let mval = fromIntegral $ proxy valueFact (Proxy::Proxy m)
+              in \(P.RLWESecret idx m s) ->
+                   if m == mval then (idx, fromProto s)
+                   else error "RLWESecret ProtoType: m values don't match"
+
+instance (Protoable (Cyc t m zq), ProtoType (Cyc t m zq) ~ Rq,
+          Protoable (UCyc t m D rq), ProtoType (UCyc t m D rq) ~ Kq)
+         => Protoable (RLWESampleCont t m zq rq) where
+
+  type ProtoType (RLWESampleCont t m zq rq) = P.RLWESampleCont
+
+  toProto (a, b) = P.RLWESampleCont (toProto a) (toProto b)
+  fromProto (P.RLWESampleCont a b) = (fromProto a, fromProto b)
+
+instance (Protoable (RLWESampleCont t m zq rq), Mod zq, Fact m)
+         => Protoable (RLWEInstanceCont Double t m zq rq) where
+
+  type ProtoType (RLWEInstanceCont Double t m zq rq) = P.RLWEInstanceCont
+
+  toProto (RLWEInstanceCont idx v bound samples) =
+    P.RLWEInstanceCont idx
+    (fromIntegral (proxy valueFact (Proxy::Proxy m)))
+    (fromIntegral (proxy modulus (Proxy::Proxy zq)))
+    v
+    bound
+    (S.fromList $ toProto <$> samples)
+
+  fromProto =
+    let mval = fromIntegral $ proxy valuefact (Proxy::Proxy m)
+        qval = fromIntegral $ proxy modulus (Proxy::Proxy zq)
+    in \ (P.RLWEInstanceCont idx m q v bound samples) ->
+         if m == mval and q == qval
+         then RLWEInstanceCont idx v bound $ fromProto <$> S.toList samples
+         else error "RLWEInstanceCont ProtoType: m or q values don't match"
