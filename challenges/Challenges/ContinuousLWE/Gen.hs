45f5fcdbd32c4b90327da6f9dafd5d901fcd71b9
Chris Peikert <cpeikert@alum.mit.edu>
2016-04-19 14:26:13 -0400

    formatting and style

--- challenges/Challenges/ContinuousLWE/Gen.hs
+++ challenges/Challenges/ContinuousLWE/Gen.hs
@@ -1,41 +1,43 @@
-{-# LANGUAGE ConstraintKinds, FlexibleContexts, 
-             NoImplicitPrelude, RebindableSyntax, TypeFamilies, ScopedTypeVariables #-}
+{-# LANGUAGE ConstraintKinds, FlexibleContexts, MultiParamTypeClasses,
+             NoImplicitPrelude, RebindableSyntax, ScopedTypeVariables,
+             TypeFamilies #-}
 
-module Challenges.ContinuousLWE.Gen 
-(lweInstance
-,module Challenges.ContinuousLWE.Proto) where
+module Challenges.ContinuousLWE.Gen
+( rlweInstance
+, module Challenges.ContinuousLWE.Proto) where
 
 import Challenges.ContinuousLWE.Proto
 
 import Control.Applicative
-import Control.Monad (replicateM)
+import Control.Monad        (replicateM)
 import Control.Monad.Random (MonadRandom, Random, getRandom)
 
-import Crypto.Lol hiding (tGaussian)
+import Crypto.Lol                   hiding (tGaussian)
 import Crypto.Lol.CRTrans
 import Crypto.Lol.Cyclotomic.Tensor
 import Crypto.Lol.Cyclotomic.UCyc
 
--- | Generate an LWE instance with a random secret, and the given scaled variance.
-lweInstance ::(ULWECtx t m z zq v rq, MonadRandom rnd, Random z)
-  => v -> Int -> rnd (Cyc t m z, [ContLWESample t m zq rq])
-lweInstance svar numSamples = do
+-- | Generate an RLWE instance with a random secret, and the given
+-- scaled variance.
+rlweInstance ::(ULWECtx t m z zq v rq, MonadRandom rnd, Random z)
+  => v -> Int -> rnd (Cyc t m z, [RLWESampleCont t m zq rq])
+rlweInstance svar numSamples = do
   s <- getRandom
   samples <- replicateM numSamples (lweSample svar s)
   return (s, samples)
 
--- | An LWE sample for a given secret.
-lweSample :: forall rnd t m z zq v rq . 
+-- | An RLWE sample for a given secret.
+rlweSample :: forall rnd t m z zq v rq .
   (MonadRandom rnd, ULWECtx t m z zq v rq)
-  => v -> Cyc t m z -> rnd (ContLWESample t m zq rq)
-lweSample svar s = do
+  => v -> Cyc t m z -> rnd (RLWESampleCont t m zq rq)
+rlweSample svar s = do
   let sq = reduce s :: Cyc t m zq
   e :: UCyc t m D (LiftOf rq) <- tGaussian svar
   a <- adviseCRT <$> getRandom
   let as = fmap fromSubgroup $ uncycDec $ a * sq :: UCyc t m D rq
-  return $ ContLWESample a $ as + (reduce e)
+  return $ RLWESampleCont a $ as + reduce e
 
-type ULWECtx t m  z zq v rq = 
+type ULWECtx t m  z zq v rq =
   (Reduce z zq, Ring zq, Random zq, Fact m, CElt t z,
    TElt t rq, Reduce (LiftOf rq) rq, OrdFloat (LiftOf rq),
    CElt t (LiftOf rq), ToRational v, Random (LiftOf rq),
--- challenges/Challenges/ContinuousLWE/Verify.hs
+++ challenges/Challenges/ContinuousLWE/Verify.hs
@@ -1,38 +1,42 @@
-{-# LANGUAGE ConstraintKinds, FlexibleContexts, NoImplicitPrelude #-}
+{-# LANGUAGE ConstraintKinds, FlexibleContexts, MultiParamTypeClasses,
+             NoImplicitPrelude #-}
 
 module Challenges.ContinuousLWE.Verify
-(checkInstance
-,CheckInst
-,module Challenges.ContinuousLWE.Proto) where
+( checkInstance
+, CheckInst
+, module Challenges.ContinuousLWE.Proto) where
 
 import Challenges.ContinuousLWE.Proto
 
-import Crypto.Lol hiding (gSqNorm)
+import Crypto.Lol                   hiding (gSqNorm)
 import Crypto.Lol.Cyclotomic.Tensor
 import Crypto.Lol.Cyclotomic.UCyc
 
--- | Verify that the (scaled, squared) norm of the noise for each sample in the instance is below @bound@.
+-- | Verify that the (scaled, squared) norm of the noise for each
+-- sample in the instance is below @bound@.
 checkInstance :: (CheckInst t m z zq rq)
-  => LWESecret t m z -> ContLWEInstance (LiftOf rq) t m zq rq -> Bool
-checkInstance (LWESecret _ s) (ContLWEInstance _ _ bound samples) = all (checkSample bound s) samples
+  => LWESecret t m z -> RLWEInstanceCont (LiftOf rq) t m zq rq -> Bool
+checkInstance (LWESecret _ s) (RLWEInstanceCont _ _ bound samples) = all (checkSample bound s) samples
 
--- | Verify that the (scaled, squared) norm of the noise for an LWE sample is below the provided bound.
-checkSample :: (CheckInst t m z zq rq) 
-  => LiftOf rq -> Cyc t m z -> ContLWESample t m zq rq -> Bool
-checkSample bound sk pair@(ContLWESample a b) = (sampleError sk pair) < bound
+-- | Verify that the (scaled, squared) norm of the noise for an LWE
+-- sample is below the provided bound.
+checkSample :: (CheckInst t m z zq rq)
+  => LiftOf rq -> Cyc t m z -> RLWESampleCont t m zq rq -> Bool
+checkSample bound sk pair@(RLWESampleCont a b) = sampleError sk pair < bound
 
-type CheckInst t m z zq rq = 
-  (Subgroup zq rq, Fact m, Ring zq,  CElt t zq,
+type CheckInst t m z zq rq =
+  (Subgroup zq rq, Fact m, Ring zq, CElt t zq,
    Reduce z zq, CElt t z,
-   Lift' rq, TElt t rq, TElt t (LiftOf rq), 
+   Lift' rq, TElt t rq, TElt t (LiftOf rq),
    Ord (LiftOf rq),
    Ring (LiftOf rq),
    UCRTElt t (LiftOf rq))
 
--- | Given an instance and corresponding secret, outputs the (scaled, squared) norm of the error term.
-sampleError :: (CheckInst t m z zq rq) 
-  => Cyc t m z -> ContLWESample t m zq rq -> LiftOf rq
-sampleError sk (ContLWESample a b) = 
-  let as = fmap fromSubgroup $ uncycDec $ a * reduce sk
+-- | Given an instance and corresponding secret, outputs the (scaled,
+-- squared) norm of the error term.
+sampleError :: (CheckInst t m z zq rq)
+  => Cyc t m z -> RLWESampleCont t m zq rq -> LiftOf rq
+sampleError sk (RLWESampleCont a b) =
+  let as = fromSubgroup <$> uncycDec $ a * reduce sk
       e = lift $ b - as
   in gSqNorm e
