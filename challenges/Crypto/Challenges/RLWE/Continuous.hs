e0ee04f5c1f57f84eeb194d6edeeff26e6f97280
Chris Peikert <cpeikert@alum.mit.edu>
2016-04-20 00:16:13 -0400

    cleaned constraints in Continuous

--- challenges/Crypto/Challenges/RLWE/Continuous.hs
+++ challenges/Crypto/Challenges/RLWE/Continuous.hs
@@ -13,15 +13,15 @@ import Control.Monad.Random
 -- | A continuous RLWE sample @(a,b) \in R_q \times K/qR@.
 type Sample t m zq rrq = (Cyc t m zq, UCyc t m D rrq)
 
-type SampleCtx t m zq rrq =
-  (Fact m, Ring zq, Random zq, Subgroup zq rrq, CElt t zq,
-   Lift' rrq, Random (LiftOf rrq), OrdFloat (LiftOf rrq),
-   TElt t rrq, TElt t (LiftOf rrq))
+type RLWECtx t m zq rrq =
+  (Fact m, Ring zq, CElt t zq, Subgroup zq rrq,
+   Lift' rrq, OrdFloat (LiftOf rrq), TElt t rrq, TElt t (LiftOf rrq))
 
--- | Generate an RLWE instance along with its (uniformly random)
--- secret, using the given scaled variance and number of desired
--- samples.
-instanceN :: (SampleCtx t m zq rrq, MonadRandom rnd, ToRational v)
+-- | Generate a continuous RLWE instance along with its (uniformly
+-- random) secret, using the given scaled variance and number of
+-- desired samples.
+instanceN :: (RLWECtx t m zq rrq, Random zq, Random (LiftOf rrq),
+              MonadRandom rnd, ToRational v)
   => v -> Int -> rnd (Cyc t m zq, [Sample t m zq rrq])
 instanceN svar num = do
   s <- getRandom
@@ -31,7 +31,8 @@ instanceN svar num = do
 -- | A continuous RLWE sample with the given scaled variance and
 -- secret.
 sample :: forall rnd v t m zq rrq .
-  (SampleCtx t m zq rrq, MonadRandom rnd, ToRational v)
+  (RLWECtx t m zq rrq, Random zq, Random (LiftOf rrq),
+   MonadRandom rnd, ToRational v)
   => v -> Cyc t m zq -> rnd (Sample t m zq rrq)
 sample svar s = do
   a <- getRandom
@@ -39,20 +40,15 @@ sample svar s = do
   let as = fmapDec fromSubgroup $ uncycDec $ a * s :: UCyc t m D rrq
   return (a, as + reduce e)
 
-type ValidCtx t m zq rrq =
-  (Subgroup zq rrq, Fact m, Ring zq, CElt t zq,
-   Lift' rrq, TElt t rrq, TElt t (LiftOf rrq),
-   Ring (LiftOf rrq), Ord (LiftOf rrq))
-
--- | Test if 'gSqNorm' of the error for each RLWE sample in the
+-- | Test if the 'gSqNorm' of the error for each RLWE sample in the
 -- instance (given the secret) is less than the given bound.
-validInstance :: (ValidCtx t m zq rrq)
+validInstance :: (RLWECtx t m zq rrq)
   => LiftOf rrq -> Cyc t m zq -> [Sample t m zq rrq] -> Bool
 validInstance bound s = all (validSample bound s)
 
 -- | Test if the 'gSqNorm' of the error for an RLWE sample (given the
 -- secret) is less than the provided bound.
-validSample :: (ValidCtx t m zq rrq)
+validSample :: (RLWECtx t m zq rrq)
   => LiftOf rrq -> Cyc t m zq -> Sample t m zq rrq -> Bool
 validSample bound s (a,b) = err < bound
   where err = let as = fmapDec fromSubgroup $ uncycDec $ a * s
