101dda2d0f6d6faf786d05be390137503474e592
Chris Peikert <cpeikert@alum.mit.edu>
2016-04-21 10:37:49 -0400

    simplified C.L.T.Random

--- lol/Crypto/Lol/Types/Random.hs
+++ lol/Crypto/Lol/Types/Random.hs
@@ -1,55 +1,34 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving, PackageImports #-}
 
--- | Defines a newtype wrapper for crypto-api's 'CryptoRandomGen', and a corresponding 'RandomGen' instance.
---   'CryptoRandomGen' generators can only be used to get 'ByteString's; the 'RandomGen' instance allows them
---   to be used to generate any 'Random' type. Typical usage is with Control.Monad.Random's 'RandT' monad.
+-- | Defines a newtype wrapper 'CryptoRand' for crypto-api's
+-- 'CryptoRandomGen', and a corresponding 'RandomGen' wrapper
+-- instance.  These are needed because 'CryptoRandomGen' generators
+-- can only be used to get 'ByteString's; the 'RandomGen' wrapper
+-- instance allows them to be used to generate any 'Random' type.
 
 module Crypto.Lol.Types.Random (CryptoRand, evalCryptoRandIO) where
 
-import Control.Monad.Random      (Rand, evalRand)
+import Control.Arrow
+import Control.Monad.CryptoRandom
+import Control.Monad.IO.Class
+import Control.Monad.Random       (RandT, evalRandT)
 import "crypto-api" Crypto.Random
-import Data.Binary.Get
-import Data.ByteString hiding (pack)
-import Data.ByteString.Lazy (pack)
-import Data.Proxy
 import System.Random
-
--- This function keeps all of the code in the IO monad, which helps write clean code below
--- No sequencing occurs between separate calls to this function.
-evalCryptoRandIO :: (CryptoRandomGen gen) => Proxy gen -> Rand (CryptoRand gen) a -> IO a
-evalCryptoRandIO _ x = do
-  gen <- newGenIO -- uses system entropy
-  return $ evalRand x gen
 
 newtype CryptoRand g = CryptoRand g deriving (CryptoRandomGen)
 
-intBytes = 
-  let bits = intLog 2 (1 + fromIntegral (maxBound :: Int) -
-                             fromIntegral (minBound :: Int) :: Integer)
-  in if (bits `mod` 8) == 0 then bits `div` 8 
-     else error "invalid Int bits in `intBytes`"
+-- | Evaluate a 'RandT' computation using a cryptographic generator
+-- @g@, seeded by system entropy.  Note that the updated generator is
+-- not returned.
+evalCryptoRandIO :: (CryptoRandomGen g, MonadIO io) => RandT g io a -> io a
+evalCryptoRandIO x = do
+  gen <- liftIO newGenIO -- uses system entropy
+  evalRandT x gen
 
-bytesToInt :: ByteString -> Int
-bytesToInt bs = case intBytes of
-  4 -> fromIntegral $ runGet getWord32host $ pack $ unpack bs
-  8 -> fromIntegral $ runGet getWord64host $ pack $ unpack bs
-  _ -> error "Unsupported Int size in `bytesToInt`"
-
--- | Yield @log_b(n)@ when it is a non-negative integer (otherwise
--- error).
-intLog :: (Integral i) => i -> i -> Int
-intLog _ 1 = 0
--- correct because ceil (lg (x)) == ceil (log (ceil (x)))
-intLog b n | (n `mod` b) == 0 = 1 + intLog b (n `div` b)
-           | otherwise = error "invalid arguments to intLog"
-
+-- "standard" RNG interface wrapper for a cryptographic RNG
 instance (CryptoRandomGen g) => RandomGen (CryptoRand g) where
-  next (CryptoRand g) = case genBytes intBytes g of
-    Right (bs, g') -> (bytesToInt bs, CryptoRand g')
-    Left err -> error $ "Next error: " ++ show err
+  -- use 'CRandom' instance for 'Int'
+  next (CryptoRand g) = either (error . show) (second CryptoRand) $ crandom g
 
-  genRange _ = (minBound, maxBound)
-
-  split (CryptoRand g) = case splitGen g of
-    Right (g1, g2) -> (CryptoRand g1, CryptoRand g2)
-    Left err -> error $ "Split error: " ++ show err
+  split (CryptoRand g) =
+    either (error . show) (CryptoRand *** CryptoRand) $ splitGen g
