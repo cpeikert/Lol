f5bb1bca48aea35c9a53b4d7a04ccf946d813ccc
Chris Peikert <cpeikert@alum.mit.edu>
2016-04-21 17:29:58 -0400

    initial options framework

--- challenges/Main.hs
+++ challenges/Main.hs
@@ -2,5 +2,53 @@
 
 module Main where
 
+import Control.Applicative
+import Options
+
+data Action = Generate | Reveal | Verify
+
+data MainOpts =
+  MainOpts
+  { optChallDir :: FilePath -- ^ location of challenges
+  }
+
+instance Options MainOpts where
+  defineOptions = MainOpts <$>
+    simpleOption "challenge-dir" "challenges/" "Path to challenges"
+
+data GenOpts =
+  GenOpts
+  { optParamsFile   :: FilePath, -- ^ file containing parameters for generation
+    optNumInstances :: Int -- ^ number of instances per challenge
+  }
+
+instance Options GenOpts where
+  defineOptions = GenOpts <$>
+    simpleOption "params" "params.txt" "File containing RLWE/R parameters" <*>
+    simpleOption "num-instances" 16
+    "Number N of instances per challenge, N = 2^k <= 256"
+
+data RevOpts =
+  RevOpts
+  { -- ??
+  }
+
+data VerOpts =
+  VerOpts
+  { -- ??
+  }
+
 main :: IO ()
-main = error "TODO"
+main = runSubcommand
+    [ subcommand "generate" generate
+    , subcommand "reveal" reveal
+    , subcommand "verify" verify
+    ]
+
+generate :: MainOpts -> GenOpts -> [String] -> IO ()
+reveal :: MainOpts -> RevOpts -> [String] -> IO ()
+verify :: MainOpts -> VerOpts -> [String] -> IO ()
+
+-- hello :: MainOptions -> HelloOpts -> [String] -> IO ()
+-- hello mainOpts opts args = unless (optQuiet mainOpts) $ do
+--     putStrLn (optHello opts)
