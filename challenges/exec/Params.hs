fb6825e75ef323ae1c4b86375a602fac7b298d76
Eric <ecrockett0@gmail.com>
2016-04-29 12:57:32 -0400

    More robust error handling when parsing params file

--- challenges/exec/Params.hs
+++ challenges/exec/Params.hs
@@ -4,6 +4,7 @@ module Params where
 
 import Common (InstanceID)
 
+import Control.Applicative hiding ((<|>))
 import Control.Monad.Except
 import Data.Int
 import Prelude hiding (lex)
@@ -42,6 +43,11 @@ lang = LanguageDef
    reservedOpNames = [],
    caseSensitive = True}
 
+-- applies `p` zero or more times, stopping when it reaches EOF
+-- if an error occurs, it stops parsing and reports the error
+manyError :: (Stream s m Char) => ParsecT s u m a -> ParsecT s u m [a]
+manyError p = (try $ eof *> return []) <|> (liftA2 (:) p (manyError p))
+
 lex :: (Stream s m Char) => ParsecT s u m a -> ParsecT s u m a
 lex = lexeme langParser
 
@@ -57,10 +63,12 @@ parseDouble = lex $ float langParser
 parseWord ::  (Monad m, Stream s m Char) => String -> ParsecT s u m ()
 parseWord =  lex . void . try . string
 
+
 paramsFile :: (MonadError String m, Stream s m Char) => ParsecT s InstanceID m [ChallengeParams]
 paramsFile = do
   whiteSpace langParser -- skip leading whitespace
-  many line
+  manyError line
+
 line :: (MonadError String m, Stream s m Char) => ParsecT s InstanceID m ChallengeParams
 line = rlwecParams <|> rlwedParams <|> rlwrParams <?> "Expected one of '" ++
   show contLineID ++ "', '" ++
@@ -68,7 +76,7 @@ line = rlwecParams <|> rlwedParams <|> rlwrParams <?> "Expected one of '" ++
   show rlwrLineID ++ "'."
 
 rlwecParams, rlwedParams, rlwrParams ::
-  (Stream s m Char) => ParsecT s InstanceID m ChallengeParams
+  (MonadError String m, Stream s m Char) => ParsecT s InstanceID m ChallengeParams
 rlwecParams = do
   parseWord contLineID
   numSamples <- parseIntegral
@@ -99,8 +107,8 @@ rlwrParams = do
   p <- parseIntegral
 
   numInsts <- getState
-  --when (p > q) $ throwError $
-  --  "Expected p <= q; parsed q=" ++ show q ++ " and p=" ++ show p
+  when (p > q) $ throwError $
+    "Expected p <= q; parsed q=" ++ show q ++ " and p=" ++ show p
   return R{..}
 
 parseChallParams :: String -> InstanceID -> [ChallengeParams]
@@ -110,4 +118,4 @@ parseChallParams input numInsts = do
     Left e -> error $ "Invalid parameters:" ++ e
     Right r -> case r of
       Left e -> error $ "Error parsing input:" ++ show e
-      Right v -> error $ show v
+      Right v -> v
