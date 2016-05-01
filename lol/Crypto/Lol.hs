07fcb5507a5800952154947c7631de28d3f19f42
Chris Peikert <cpeikert@alum.mit.edu>
2016-05-01 11:56:03 -0400

    hlint imports

--- lol/Crypto/Lol.hs
+++ lol/Crypto/Lol.hs
@@ -1,16 +1,16 @@
 
 -- | Re-exports primary interfaces.
 
-module Crypto.Lol 
+module Crypto.Lol
 ( module X
 ) where
 
 import Crypto.Lol.Cyclotomic.Cyc as X
-import Crypto.Lol.Gadget as X
+import Crypto.Lol.Gadget         as X
 import Crypto.Lol.LatticePrelude as X
 
-import Crypto.Lol.Types.RRq as X
-import Crypto.Lol.Types.ZqBasic as X
-import Crypto.Lol.Cyclotomic.Tensor.CTensor as X
+import Crypto.Lol.Cyclotomic.Tensor.CTensor    as X
 import Crypto.Lol.Cyclotomic.Tensor.RepaTensor as X
-import Crypto.Lol.Types.IrreducibleChar2 as X
+import Crypto.Lol.Types.IrreducibleChar2       as X ()
+import Crypto.Lol.Types.RRq                    as X
+import Crypto.Lol.Types.ZqBasic                as X
