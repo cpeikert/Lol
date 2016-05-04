97be676332066bd7772ecf01cef5a9711e333d09
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2016-05-04 15:16:42 +1000

    Just reorganising
    Putting the CRTrans and CRTEmbed things together

--- lol/Crypto/Lol/CRTrans.hs
+++ lol/Crypto/Lol/CRTrans.hs
@@ -1,10 +1,16 @@
-{-# LANGUAGE FlexibleContexts, FlexibleInstances, MultiParamTypeClasses,
-             PolyKinds, RebindableSyntax, ScopedTypeVariables,
-             TypeFamilies #-}
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE PolyKinds             #-}
+{-# LANGUAGE RebindableSyntax      #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE UndecidableInstances  #-}
 
--- | Classes and helper methods for the Chinese remainder transform
--- and ring extensions.
-
+-- | Classes and helper methods for the Chinese remainder transform and ring
+-- extensions.
+--
 module Crypto.Lol.CRTrans
 ( CRTrans(..), CRTEmbed(..)
 , CRTInfo
@@ -16,6 +22,9 @@ import Crypto.Lol.Reflects
 import Control.Arrow
 
 
+-- CRTrans
+-- -----------------------------------------------------------------------------
+
 -- | Information that characterizes the (invertible) Chinese remainder
 -- transformation over a ring @r@, namely:
 --
@@ -23,17 +32,18 @@ import Control.Arrow
 --     @m@th root of unity (for any integer @i@)
 --
 --     (2) the multiplicative inverse of @\\hat{m}@ in @r@.
-
+--
 type CRTInfo i r = (i -> r, r)
+
 
 -- | A ring that (possibly) supports invertible Chinese remainder
 -- transformations of various indices.
-
--- | The values of 'crtInfo' for different indices @m@ should be
--- consistent, in the sense that if @omega@, @omega'@ are respectively
--- the roots of unity used for @m@, @m'@ where @m'@ divides @m@, then
--- it should be the case that @omega^(m/m')=omega'@.
-
+--
+-- The values of 'crtInfo' for different indices @m@ should be consistent, in
+-- the sense that if @omega@, @omega'@ are respectively the roots of unity used
+-- for @m@, @m'@ where @m'@ divides @m@, then it should be the case that
+-- @omega^(m/m')=omega'@.
+--
 class (Monad mon, Ring r) => CRTrans mon i r where
 
   -- | 'CRTInfo' for a given index @m@. The method itself may be
@@ -41,15 +51,6 @@ class (Monad mon, Ring r) => CRTrans mon i r where
   -- internal memoization.
   crtInfo :: Reflects m i => TaggedT m mon (CRTInfo i r)
 
--- | A ring with a ring embedding into some ring @CRTExt r@ that has
--- an invertible CRT transformation for /every/ positive index @m@.
-class (Ring r, Ring (CRTExt r)) => CRTEmbed r where
-  type CRTExt r
-
-  -- | Embeds from @r@ to @CRTExt r@
-  toExt :: r -> CRTExt r
-  -- | Projects from @CRTExt r@ to @r@
-  fromExt :: CRTExt r -> r
 
 -- CRTrans instance for product rings
 instance (CRTrans mon i a, CRTrans mon i b) => CRTrans mon i (a,b) where
@@ -58,18 +59,22 @@ instance (CRTrans mon i a, CRTrans mon i b) => CRTrans mon i (a,b) where
     (fb, invb) <- crtInfo
     return (fa &&& fb, (inva, invb))
 
--- CRTEmbed instance for product rings
-instance (CRTEmbed a, CRTEmbed b) => CRTEmbed (a,b) where
-  type CRTExt (a,b) = (CRTExt a, CRTExt b)
-  toExt   = toExt *** toExt
-  fromExt = fromExt *** fromExt
+-- CRTrans instances for real and integer types, which do not have roots of
+-- unity (except in trivial cases). These are needed to use Cyc with these
+-- integral types.
+instance CRTrans Maybe Int Double  where crtInfo = tagT Nothing
+instance CRTrans Maybe Int Int     where crtInfo = tagT Nothing
+instance CRTrans Maybe Int Int64   where crtInfo = tagT Nothing
+instance CRTrans Maybe Int Integer where crtInfo = tagT Nothing
+-- can also do for Int8, Int16, Int32 etc.
 
 -- the complex numbers have roots of unity of any order
 instance (Monad mon, Transcendental a) => CRTrans mon Int (Complex a) where
   crtInfo = crtInfoC
 
-crtInfoC :: forall mon m a . (Monad mon, Reflects m Int, Transcendental a)
-            => TaggedT m mon (CRTInfo Int (Complex a))
+crtInfoC
+    :: forall mon m a . (Monad mon, Reflects m Int, Transcendental a)
+    => TaggedT m mon (CRTInfo Int (Complex a))
 crtInfoC = let mval = proxy value (Proxy::Proxy m)
                mhat = valueHat mval
            in return (omegaPowC mval, recip $ fromIntegral mhat)
@@ -77,20 +82,35 @@ crtInfoC = let mval = proxy value (Proxy::Proxy m)
 omegaPowC :: (Transcendental a) => Int -> Int -> Complex a
 omegaPowC m i = cis (2*pi*fromIntegral i / fromIntegral m)
 
+
+-- CRTEmbed
+-- -----------------------------------------------------------------------------
+
+-- | A ring with a ring embedding into some ring @CRTExt r@ that has an
+-- invertible CRT transformation for /every/ positive index @m@.
+--
+class (Ring r, Ring (CRTExt r)) => CRTEmbed r where
+  type CRTExt r
+
+  -- | Embeds from @r@ to @CRTExt r@
+  toExt   :: r -> CRTExt r
+
+  -- | Projects from @CRTExt r@ to @r@
+  fromExt :: CRTExt r -> r
+
+
+-- CRTEmbed instance for product rings
+instance (CRTEmbed a, CRTEmbed b) => CRTEmbed (a,b) where
+  type CRTExt (a,b) = (CRTExt a, CRTExt b)
+  toExt   = toExt *** toExt
+  fromExt = fromExt *** fromExt
+
 -- trivial CRTEmbed instance for complex numbers
-instance (Transcendental a) => CRTEmbed (Complex a) where
+instance Transcendental a => CRTEmbed (Complex a) where
   type CRTExt (Complex a) = Complex a
   toExt   = id
   fromExt = id
 
--- CRTrans instances for real and integer types, which do
--- not have roots of unity (except in trivial cases). These are needed
--- to use Cyc with these integral types.
-instance CRTrans Maybe Int Double  where crtInfo = tagT Nothing
-instance CRTrans Maybe Int Int     where crtInfo = tagT Nothing
-instance CRTrans Maybe Int Int64   where crtInfo = tagT Nothing
-instance CRTrans Maybe Int Integer where crtInfo = tagT Nothing
--- can also do for Int8, Int16, Int32 etc.
 
 -- CRTEmbed instances for real and integer types, embedding into
 -- Complex.  These are needed to use Cyc with these integer types.
