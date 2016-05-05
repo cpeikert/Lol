c45a6bfd82e8decad58bac824a8176eb837d65fa
Eric <ecrockett0@gmail.com>
2016-05-05 11:27:43 -0400

    Fixed stack ghci and ghc bug on OSX

--- lol/Setup.hs
+++ lol/Setup.hs
@@ -18,40 +18,38 @@ main = defaultMainWithHooks simpleUserHooks
 
 makeExtLib :: Args -> ConfigFlags -> IO HookedBuildInfo
 makeExtLib _ flags = do
-    let verbosity = fromFlag $ configVerbosity flags
-    rawSystemExit verbosity "env"
-        ["make", "--directory=."]
-    return emptyHookedBuildInfo
+  let verbosity = fromFlag $ configVerbosity flags
+  rawSystemExit verbosity "env"
+    ["make", "--directory=."]
+  return emptyHookedBuildInfo
 
 updateExtraLibDirs :: LocalBuildInfo -> IO LocalBuildInfo
 updateExtraLibDirs localBuildInfo = do
-    let packageDescription = localPkgDescr localBuildInfo
-        lib = fromJust $ library packageDescription
-        libBuild = libBuildInfo lib
-    dir <- getCurrentDirectory
-    return localBuildInfo {
-        localPkgDescr = packageDescription {
-            library = Just $ lib {
-                libBuildInfo = libBuild {
-                    extraLibDirs = (dir ++ "/dist/build/staticlib") :
-                        extraLibDirs libBuild
-                }
-            }
+  let packageDescription = localPkgDescr localBuildInfo
+      lib = fromJust $ library packageDescription
+      libBuild = libBuildInfo lib
+  dir <- getCurrentDirectory
+  return localBuildInfo {
+    localPkgDescr = packageDescription {
+      library = Just $ lib {
+        libBuildInfo = libBuild {
+          extraLibDirs = (dir ++ "/dist/build/staticlib") : extraLibDirs libBuild
         }
+      }
     }
+  }
 
 copyExtLib :: Args -> CopyFlags -> PackageDescription -> LocalBuildInfo -> IO ()
 copyExtLib _ flags pkg_descr lbi = do
-    let libPref = libdir . absoluteInstallDirs pkg_descr lbi
-                . fromFlag . copyDest
-                $ flags
-    let verbosity = fromFlag $ copyVerbosity flags
-    -- copies from the output of makefile to libPref
-    -- make sure the path below matches the makefile output file
-    rawSystemExit verbosity "cp" ["dist/build/staticlib/libctensor.a", libPref]
-    rawSystemExit verbosity "cp" ["dist/build/dynamiclib/libctensor.so", libPref]
+  let libPref =
+        libdir . absoluteInstallDirs pkg_descr lbi . fromFlag . copyDest $ flags
+  let verbosity = fromFlag $ copyVerbosity flags
+  -- copies from the output of makefile to libPref
+  -- make sure the path below matches the makefile output file
+  rawSystemExit verbosity "cp" ["dist/build/staticlib/libctensor.a", libPref]
+  rawSystemExit verbosity "cp" ["dist/build/dynamiclib/libctensor.so", libPref]
 
 cleanExtLib :: Args -> CleanFlags -> PackageDescription -> () -> IO ()
 cleanExtLib _ flags _ _ =
-    let verbosity = fromFlag $ cleanVerbosity flags
-    in rawSystemExit verbosity "env" ["make", "--directory=.", "clean"]
+  let verbosity = fromFlag $ cleanVerbosity flags
+  in rawSystemExit verbosity "env" ["make", "--directory=.", "clean"]
