8b7388ce65664c6b6c62b44cc9281ca3ecfa34df
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2016-05-12 09:24:18 +1000

    wibble

--- lol/Crypto/Lol/Cyclotomic/Cyc.hs
+++ lol/Crypto/Lol/Cyclotomic/Cyc.hs
@@ -514,7 +514,7 @@ instance ( Mod a, Field b, Lift a (ModRep a), Reduce (LiftOf a) b
          , CElt t (a,b), CElt t a, CElt t b, CElt t (LiftOf a)
          , Lift (TRep t a) (TRep t b)
          , Reduce (LiftOf (TRep t a)) (TRep t b)
-         , LiftOf (TRep t a) ~ TRep t (LiftOf a)
+         , LiftOf (TRep t a) ~ TRep t (LiftOf a)  -- TLM: hmm...
          , CRTExt (a,b) ~ (CRTExt a, CRTExt b)
          )
          => R.RescaleCyc (Cyc t) (a,b) b where
--- lol/Crypto/Lol/Cyclotomic/UCyc.hs
+++ lol/Crypto/Lol/Cyclotomic/UCyc.hs
@@ -276,12 +276,14 @@ instance (Tensor t, Reduce a b, Reduce (TRep t a) (TRep t b), Fact m, TElt t a, 
 type instance LiftOf (UCyc t m P r) = UCyc t m P (LiftOf r)
 type instance LiftOf (UCyc t m D r) = UCyc t m D (LiftOf r)
 
-instance (Tensor t, Lift' r, Lift' (TRep t r), Fact m, TElt t r, TElt t (LiftOf r), TRep t (LiftOf r) ~ LiftOf (TRep t r))
+instance ( Tensor t, Lift' r, Lift' (TRep t r), Fact m, TElt t r, TElt t (LiftOf r)
+         , TRep t (LiftOf r) ~ LiftOf (TRep t r)) -- TLM: hmm...
          => Lift' (UCyc t m P r) where
   lift (Pow v) = Pow $ fmapT lift v
   {-# INLINABLE lift #-}
 
-instance (Tensor t, Lift' r, Lift' (TRep t r), Fact m, TElt t r, TElt t (LiftOf r), TRep t (LiftOf r) ~ LiftOf (TRep t r))
+instance ( Tensor t, Lift' r, Lift' (TRep t r), Fact m, TElt t r, TElt t (LiftOf r)
+         , TRep t (LiftOf r) ~ LiftOf (TRep t r)) -- TLM: hmm...
          => Lift' (UCyc t m D r) where
   lift (Dec v) = Dec $ fmapT lift v
   {-# INLINABLE lift #-}
