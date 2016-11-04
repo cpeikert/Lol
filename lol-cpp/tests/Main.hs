a6c78db6fda2598febd3b6625e3b1e7b6f92544b
Eric <ecrockett0@gmail.com>
2016-11-04 14:25:53 -0400

    Added tests for cpp/repa/easy

---
+++ lol-cpp/tests/Main.hs
@@ -0,0 +1,7 @@
+
+import Crypto.Lol.Cyclotomic.Tensor.CPP
+import Crypto.Lol.Tests.Standard
+import Data.Proxy
+
+main :: IO ()
+main = defaultTestMain (Proxy::Proxy CT)
---
+++ lol-repa/tests/Main.hs
@@ -0,0 +1,7 @@
+
+import Crypto.Lol.Cyclotomic.Tensor.Repa
+import Crypto.Lol.Tests.Standard
+import Data.Proxy
+
+main :: IO ()
+main = defaultTestMain (Proxy::Proxy RT)
