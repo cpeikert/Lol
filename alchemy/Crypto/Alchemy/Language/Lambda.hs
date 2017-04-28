fa6d43d0166c117e23288342791ec7af24f62bb9
Chris Peikert <cpeikert@alum.mit.edu>
2017-04-28 15:56:15 -0400

    initial Lambda/de Bruijn symantics

---
+++ alchemy/Crypto/Alchemy/Language/Lambda.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+module Crypto.Alchemy.Language.Lambda where
+
+-- | Symantics for functions and application.
+
+class Lambda expr where
+  -- | Abstraction.
+  lam  :: expr (e,a) b -> expr e (a -> b)
+
+  -- | Application.
+  ($:) :: expr e (a -> b) -> expr e a -> expr e b
+
+-- | Let-sharing.
+let_ :: Lambda expr => expr e a -> expr (e,a) b -> expr e b
+let_ a f = lam f $: a
+
+-- | Symantics for de Bruijn variables.
+
+class DB expr a where
+  -- | The zero'th (most recently bound) variable.
+  v0 :: expr (b,a) a
+
+  -- | The next-most-recently-bound variable from the given one.
+  s  :: expr e a -> expr (e,x) a
+
+-- CJP: for some reason have to give signature here, even though ghci
+-- infers them correctly
+
+v1 :: DB expr b => expr ((c,b),a) b
+v1 = s v0
+
+v2 :: DB expr c => expr (((d,c),b),a) c
+v2 = s v1
+
+v3 :: DB expr d => expr ((((e,d),c),b),a) d
+v3 = s v2
+
+v4 :: DB expr e => expr (((((f,e),d),c),b),a) e
+v4 = s v3
