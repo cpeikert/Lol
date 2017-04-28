09a7c4c460309a72e8ebe6bf1594e05b0ba9d498
Chris Peikert <cpeikert@alum.mit.edu>
2017-04-28 15:56:36 -0400

    initial Arithmetic symantics

---
+++ alchemy/Crypto/Alchemy/Language/Arithmetic.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE MultiParamTypeClasses  #-}
+
+module Crypto.Alchemy.Language.Arithmetic where
+
+-- | Addition.
+
+class Add expr a where
+  (+#) :: expr e a -> expr e a -> expr e a
+
+-- | Multiplication. (Note that the input type @b@ may differ from the
+-- output type @a@.)
+
+class Mul expr a b | a -> b where
+  (*#) :: expr e b -> expr e b -> expr e a
