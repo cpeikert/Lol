16d75dbf13dcbc8e572e35bc8728690d598468de
Chris Peikert <cpeikert@alum.mit.edu>
2017-04-28 16:47:15 -0400

    pretty-printer

---
+++ alchemy/Crypto/Alchemy/Interpreter/Print.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+module Crypto.Alchemy.Interpreter.Print
+( P, pprint
+)
+where
+
+import Crypto.Alchemy.Language.Arithmetic
+import Crypto.Alchemy.Language.Lambda
+
+-- the Int is the nesting depth of lambdas outside the expression
+newtype P e a = P { unP :: Int -> String }
+
+-- | Pretty-print a closed expression.
+pprint :: P () a -> String
+pprint = flip unP 0
+
+-- CJP: variable indices increase ("outside in"), rather than "inside
+-- out" (the object-language form) because it is a simpler
+-- implementation.
+
+instance DB P a where
+  v0   = P $ \i -> "v" ++ show (i-1)
+  s  v = P $ \i -> unP v (i-1)
+
+instance Lambda P where
+  lam f  = P $ \i -> "(λv" ++ show i ++ " → " ++ unP f (i+1) ++ ")"
+  f $: a = P $ \i -> "(" ++ unP f i ++  " " ++ unP a i ++ ")"
+
+instance Add P a where
+  a +# b = P $ \i -> "(" ++ unP a i ++ " + " ++ unP b i ++ ")"
+
+instance Mul P a a where
+  a *# b = P $ \i -> "(" ++ unP a i ++ " * " ++ unP b i ++ ")"
+
