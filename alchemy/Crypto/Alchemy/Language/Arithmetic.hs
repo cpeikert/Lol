5a62fa4c6f2888d609d2fa4b7c0facb6e889f0ab
Chris Peikert <cpeikert@alum.mit.edu>
2017-04-28 23:58:09 -0400

    added fixity for +:, *:

--- alchemy/Crypto/Alchemy/Language/Arithmetic.hs
+++ alchemy/Crypto/Alchemy/Language/Arithmetic.hs
@@ -6,10 +6,14 @@ module Crypto.Alchemy.Language.Arithmetic where
 -- | Addition.
 
 class Add expr a where
+  infixl 6 +:                   -- match Haskell's precedence
+
   (+:) :: expr e a -> expr e a -> expr e a
 
 -- | Multiplication. (Note that the input type @b@ may differ from the
 -- output type @a@.)
 
 class Mul expr b a | a -> b where
+  infixl 7 *:                   -- match Haskell's precedence
+
   (*:) :: expr e b -> expr e b -> expr e a
