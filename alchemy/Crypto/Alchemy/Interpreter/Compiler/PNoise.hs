ec4bd6ea04af7a922f20ea6be28c775c275f9a06
Eric <ecrockett0@gmail.com>
2017-04-30 13:56:15 -0400

    Removed comments from PNoise

--- alchemy/Crypto/Alchemy/Interpreter/Compiler/PNoise.hs
+++ alchemy/Crypto/Alchemy/Interpreter/Compiler/PNoise.hs
@@ -15,9 +15,8 @@
 {-# LANGUAGE TypeOperators              #-}
 {-# LANGUAGE UndecidableInstances       #-}
 
-module Crypto.Alchemy.Interpreter.Compiler.PNoise 
--- CJP: need an explicit export list?
-where
+-- should be a hidden/internal module
+module Crypto.Alchemy.Interpreter.Compiler.PNoise where
 
 import           Algebra.Additive          as Additive (C)
 import           Algebra.Ring              as Ring (C)
@@ -48,9 +47,6 @@ type family L2P a where
   L2P '[a] = a
   L2P (a ': b) = (a, List2Pairs b)
 
--- CJP: can this ad-hoc MapModulus be eliminated by using singletons
--- type-level Map?
-
 -- | Maps  'Modulus' over a type list.
 type family MapModulus zqs where
   MapModulus '[] = '[]
@@ -61,8 +57,6 @@ data TLNatNat = NN TL.Nat Nat
 
 type family NatOf a where
   NatOf ('NN a b) = b
-
--- CJP: ditto: use singletons type-level Map?
 
 -- | Maps 'NatOf' over a type list.
 type family MapNatOf a where
@@ -77,12 +71,10 @@ singletons [d|
 
              -- | Given a list and a threshold h, output the length of
              -- the shortest nonempty prefix whose sum is >= h.
-
-             -- CJP: fixed a bug here -- it was never decreasing h!
              prefixLen :: [Nat] -> Nat -> Nat
              prefixLen (a : rest) h = if a >= h then S Z
                                       else S (prefixLen rest (h - a))
-             prefixLen [] _ = 
+             prefixLen [] _ =
                error "prefixLen: threshold is larger than sum of list entries"
            |]
 
@@ -90,11 +82,6 @@ singletons [d|
 -- modulus large enough to contain the given pNoise @h@.
 type PNoise2Zq zqs h =
   List2Pairs (Take (PrefixLen (MapNatOf (MapModulus zqs)) h) zqs)
-
-
--- 8 bits seems like too much, because our moduli need to be slightly
--- less than 32 bits to fit into a 64 bit modulus,
--- but we'd still like to treat them as having 4 units of noise.
 
 -- | Base of the logarithm in pNoise := -log (noise rate).
 pNoiseBase :: Double
@@ -115,4 +102,3 @@ mkQ q =
 
 instance (Reflects x i) => Reflects ('NN x y) i where
   value = retag $ value @x
-
