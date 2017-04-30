9d01029e81f931efecfa5c5b7353cdb3ed4b0514
Chris Peikert <cpeikert@alum.mit.edu>
2017-04-30 17:17:08 -0400

    updated function/constraint names, wibble

--- alchemy/Crypto/Alchemy/Interpreter/Eval.hs
+++ alchemy/Crypto/Alchemy/Interpreter/Eval.hs
@@ -5,10 +5,7 @@
 {-# LANGUAGE TypeFamilies               #-}
 {-# LANGUAGE UndecidableInstances       #-}
 
-module Crypto.Alchemy.Interpreter.Eval
-( E, eval
-)
-where
+module Crypto.Alchemy.Interpreter.Eval ( E, eval) where
 
 import Control.Applicative
 import Data.Tuple
@@ -21,11 +18,11 @@ import Crypto.Alchemy.Language.Arithmetic
 import Crypto.Alchemy.Language.Lambda
 import Crypto.Alchemy.Language.SHE
 
-import Crypto.Lol (Cyc, RescaleCyc)
+import Crypto.Lol                      (Cyc, RescaleCyc)
 import Crypto.Lol.Applications.SymmSHE as SHE
 
 -- | Metacircular evaluator.
-newtype E e a = E { unE :: e -> a } deriving (Functor,Applicative)
+newtype E e a = E { unE :: e -> a } deriving (Functor, Applicative)
 
 -- | Evaluate a closed expression (i.e., one not having any unbound
 -- variables)
@@ -44,27 +41,33 @@ instance (Additive.C a) => Add E a where
   x +: y = (+) <$> x <*> y
 
 instance (Additive.C a) => AddLit E a where
-  addLit x y = (x+) <$> y
+  addLit x y = (x +) <$> y
 
 instance (Ring.C a) => Mul E a where
   type PreMul E a = a
   x *: y = (*) <$> x <*> y
 
 instance (Ring.C a) => MulLit E a where
-  mulLit x y = (x*) <$> y
+  mulLit x y = (x *) <$> y
 
 instance SHE E where
 
-  type ModSwitchCtx E (CT m zp (Cyc t m' zq)) zp'     = (ModSwitchPTCtx t m' zp zp' zq)
-  type RescaleCtx   E (CT m zp (Cyc t m' zq)) zq'     = (RescaleCyc (Cyc t) zq' zq, ToSDCtx t m' zp zq')
-  type AddPubCtx    E (CT m zp (Cyc t m' zq))         = (AddPublicCtx t m m' zp zq)
-  type MulPubCtx    E (CT m zp (Cyc t m' zq))         = (MulPublicCtx t m m' zp zq)
-  type KeySwitchCtx E (CT m zp (Cyc t m' zq)) zq' gad = (SHE.KeySwitchCtx gad t m' zp zq zq')
-  type TunnelCtx    E t e r s e' r' s' zp zq gad      = (SHE.TunnelCtx t r s e' r' s' zp zq gad)
+  type ModSwitchPTCtx    E (CT m zp (Cyc t m' zq)) zp'     =
+    (SHE.ModSwitchPTCtx t m' zp zp' zq)
+  type RescaleLinearCtx  E (CT m zp (Cyc t m' zq)) zq'     =
+    (RescaleCyc (Cyc t) zq' zq, ToSDCtx t m' zp zq')
+  type AddPublicCtx      E (CT m zp (Cyc t m' zq))         =
+    (SHE.AddPublicCtx t m m' zp zq)
+  type MulPublicCtx      E (CT m zp (Cyc t m' zq))         =
+    (SHE.MulPublicCtx t m m' zp zq)
+  type KeySwitchQuadCtx  E (CT m zp (Cyc t m' zq)) zq' gad =
+    (SHE.KeySwitchCtx gad t m' zp zq zq')
+  type TunnelCtx E t e r s e' r' s' zp zq gad              =
+    (SHE.TunnelCtx t r s e' r' s' zp zq gad)
 
-  modSwitchPT     = fmap SHE.modSwitchPT
-  rescaleLinearCT = fmap SHE.rescaleLinearCT
-  addPublic       = fmap . SHE.addPublic
-  mulPublic       = fmap . SHE.mulPublic
-  keySwitchQuad   = fmap . SHE.keySwitchQuadCirc
-  tunnel          = fmap . SHE.tunnelCT
+  modSwitchPT    = fmap   SHE.modSwitchPT
+  rescaleLinear  = fmap   SHE.rescaleLinearCT
+  addPublic      = fmap . SHE.addPublic
+  mulPublic      = fmap . SHE.mulPublic
+  keySwitchQuad  = fmap . SHE.keySwitchQuadCirc
+  tunnel         = fmap . SHE.tunnelCT
