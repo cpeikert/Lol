fd8aef53758faf4d5ad02428a4877791df61641d
Chris Peikert <cpeikert@alum.mit.edu>
2017-04-30 13:10:42 -0400

    better names, comments, tried to fix bug (won't compile)

--- alchemy/Crypto/Alchemy/Interpreter/Compiler/PNoise.hs
+++ alchemy/Crypto/Alchemy/Interpreter/Compiler/PNoise.hs
@@ -1,97 +1,107 @@
+{-# LANGUAGE ConstraintKinds            #-}
 {-# LANGUAGE DataKinds                  #-}
 {-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GADTs                      #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE InstanceSigs               #-}
 {-# LANGUAGE KindSignatures             #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE PolyKinds                  #-}
+{-# LANGUAGE RankNTypes                 #-}
 {-# LANGUAGE ScopedTypeVariables        #-}
-{-# LANGUAGE TemplateHaskellQuotes      #-}
+{-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE TypeApplications           #-}
 {-# LANGUAGE TypeFamilies               #-}
 {-# LANGUAGE TypeOperators              #-}
+{-# LANGUAGE UndecidableInstances       #-}
 
-{-# LANGUAGE ConstraintKinds      #-}
-{-# LANGUAGE DataKinds            #-}
-{-# LANGUAGE GADTs                #-}
-{-# LANGUAGE InstanceSigs         #-}
-{-# LANGUAGE KindSignatures       #-}
-{-# LANGUAGE PolyKinds            #-}
-{-# LANGUAGE RankNTypes           #-}
---{-# LANGUAGE RebindableSyntax     #-}
-{-# LANGUAGE ScopedTypeVariables  #-}
-{-# LANGUAGE TemplateHaskell      #-}
-{-# LANGUAGE TypeFamilies         #-}
-{-# LANGUAGE UndecidableInstances #-}
+module Crypto.Alchemy.Interpreter.Compiler.PNoise 
+-- CJP: need an explicit export list?
+where
 
-module Crypto.Alchemy.Interpreter.Compiler.PNoise where
-
-import Algebra.Additive as Additive (C)
-import Algebra.Ring as Ring (C)
-import Language.Haskell.TH
-import Data.Functor.Trans.Tagged
-import Data.Singletons.Prelude
-import Data.Singletons.TH
-import Data.Type.Natural
-import qualified GHC.TypeLits as TL
+import           Algebra.Additive          as Additive (C)
+import           Algebra.Ring              as Ring (C)
+import           Data.Functor.Trans.Tagged
+import           Data.Singletons.Prelude
+import           Data.Singletons.TH
+import           Data.Type.Natural
+import qualified GHC.TypeLits              as TL
+import           Language.Haskell.TH
 
 import Crypto.Lol.Reflects
 import Crypto.Lol.Types.Unsafe.ZqBasic
 
 -- | A value tagged by @pNoise =~ -log(noise rate)@.
-newtype PNoise (h :: Nat) a = PN a deriving (Additive.C, Ring.C)
+newtype PNoise (h :: Nat) a = PN a
+  deriving (Additive.C, Ring.C)
 
--- open, to be defined possibly for other Zq reprs
+-- CJP: why should this be defined here?
 type family Modulus zq :: k
 type instance Modulus (ZqBasic q i) = q
 
--- Maps PNoise to a modulus from a list, according to a heuristic
+-- | Convert a list to nested pairs, in "reverse inside out" order:
+-- last element of list is first and outermost.
+type family List2Pairs a where List2Pairs a = L2P (Reverse a)
 
--- turns a list of moduli into a nested-pair representation
-type family List2Zq zqs where
-  List2Zq '[zq0] = zq0
-  List2Zq (zq0 ': zqs) = (zq0, List2Zq zqs)
+-- auxiliary family
+type family L2P a where
+  L2P '[a] = a
+  L2P (a ': b) = (a, List2Pairs b)
 
-type family ZqsToMods zqs where
-  ZqsToMods '[] = '[]
-  ZqsToMods (x ': xs) = (Modulus x) ': (ZqsToMods xs)
+-- CJP: can this ad-hoc MapModulus be eliminated by using singletons
+-- type-level Map?
 
-data PNoiseMod = PNM TL.Nat Nat
+-- | Maps  'Modulus' over a type list.
+type family MapModulus zqs where
+  MapModulus '[] = '[]
+  MapModulus (x ': xs) = (Modulus x) ': (MapModulus xs)
 
--- | Turn a list of PNMs into a list of Nats to be operated on by singletons
-type family LogOnly zqs where
-  LogOnly '[] = '[]
-  LogOnly (('PNM q pq) ': qs) = pq ': LogOnly qs
+-- | A type-lit 'TL.Nat' with a type-natural 'Nat'.
+data TLNatNat = NN TL.Nat Nat
+
+type family NatOf a where
+  NatOf ('NN a b) = b
+
+-- CJP: ditto: use singletons type-level Map?
+
+-- | Maps 'NatOf' over a type list.
+type family MapNatOf a where
+  MapNatOf '[] = '[]
+  MapNatOf (a ': rest) = (NatOf a) ': MapNatOf rest
 
 singletons [d|
-            -- | Version included with singletons takes a TypeLit, rather than Nat
-            take :: Nat -> [a] -> [a]
-            take Z _ = []
-            take (S n) (x : xs) = x : take n xs
+             -- | (Singletons version takes a TypeLit, rather than a 'Nat'.)
+             take :: Nat -> [a] -> [a]
+             take Z _ = []
+             take (S n) (x : xs) = x : take n xs
 
-            -- given a list of Nats representing the pNoise of moduli and an `h`,
-            -- output the number of moduli in the list to use
-            -- EAC: Singletons doesn't like guard patterns
-            pNoise2Len :: [Nat] -> Nat -> Nat
-            pNoise2Len [] _ = error "Requested h is too largle!"
-            pNoise2Len (pq : pqs) h =
-              if h <= pq
-              then S Z
-              else S (pNoise2Len pqs h)
+             -- | Given a list and a threshold h, output the length of
+             -- the shortest nonempty prefix whose sum is >= h.
+
+             -- CJP: fixed a bug here -- it was never decreasing h!
+             prefixLen :: [Nat] -> Nat -> Nat
+             prefixLen (a : rest) h = if a >= h then S Z
+                                      else S (prefixLen rest (h :- a))
+             prefixLen [] _ = 
+               error "prefixLen: threshold is larger than sum of list entries"
            |]
 
+-- | Given a list of moduli, construct nested pairs representing a
+-- modulus large enough to contain the given pNoise @h@.
 type PNoise2Zq zqs h =
-  -- compute the *number* of moduli needed, restrict the list to that many,
-  -- *reverse* it, so that when we build a modulus out of it, it's in the optimal order for RescaleCyc
-  List2Zq (Reverse (Take (PNoise2Len (LogOnly (ZqsToMods zqs)) h) zqs))
+  List2Pairs (Take (PrefixLen (MapNatOf (MapModulus zqs)) h) zqs)
 
 
 -- 8 bits seems like too much, because our moduli need to be slightly
 -- less than 32 bits to fit into a 64 bit modulus,
 -- but we'd still like to treat them as having 4 units of noise.
--- | One unit of "noise", in bits.
-noiseUnit :: Double
-noiseUnit = 7.5
+
+-- | Base of the logarithm in pNoise := -log (noise rate).
+pNoiseBase :: Double
+pNoiseBase = 7.5
 
 mkTypeNat :: Int -> TypeQ
+mkTypeNat x | x < 0 = error $ "mkTypeNat: negative argument " ++ show x
 mkTypeNat 0 = conT 'Z
 mkTypeNat x = conT 'S `appT` (mkTypeNat $ x-1)
 
@@ -100,8 +110,9 @@ mkTypeLit = litT . numTyLit
 
 mkQ :: Integer -> TypeQ
 mkQ q =
-  let lgq = floor $ logBase 2 (fromInteger q :: Double) / noiseUnit
-  in conT 'PNM `appT` (mkTypeLit q) `appT` (mkTypeNat lgq)
+  let lgq = floor $ logBase pNoiseBase (fromInteger q)
+  in conT 'NN `appT` (mkTypeLit q) `appT` (mkTypeNat lgq)
 
-instance (Reflects x i) => Reflects ('PNM x y) i where
+instance (Reflects x i) => Reflects ('NN x y) i where
   value = retag $ value @x
+
