668d38b44bb5d589f675cb83f67a65996ca22f55
Eric <ecrockett0@gmail.com>
2017-04-30 15:19:38 -0400

    Updates to top-level example. Compiles, but doesn't quite execute.

--- alchemy/Crypto/Alchemy/Examples/Arithmetic.hs
+++ alchemy/Crypto/Alchemy/Examples/Arithmetic.hs
@@ -10,7 +10,6 @@
 module Crypto.Alchemy.Examples.Arithmetic where
 
 import Control.Monad.Reader
-import Control.Monad.State
 
 import Crypto.Alchemy.Interpreter.Dup
 import Crypto.Alchemy.Interpreter.Eval
@@ -20,12 +19,13 @@ import Crypto.Alchemy.Interpreter.Compiler.PT2CT
 
 import Crypto.Alchemy.Language.Arithmetic
 import Crypto.Alchemy.Language.Lambda
-import Crypto.Alchemy.Language.Lit
 
 import Crypto.Lol hiding (Pos(..))
+import qualified Crypto.Lol.Applications.SymmSHE as SHE
 import Crypto.Lol.Cyclotomic.Tensor.CPP
 import Crypto.Lol.Types
 
+import Data.Maybe
 import Data.Type.Natural (Nat (Z))
 
 -- EAC: We can get rid of signatures once #13524 is fixed (should be in 8.2)
@@ -36,37 +36,34 @@ pt1 :: forall b e expr a .
   => expr e (a -> a -> b)
 pt1 = lam $ lam $ v0 *: (v0 +: (s v0))
 
-pt1' :: forall b e expr a .
-  (a ~ PreMul expr b, Mul expr b, Add expr a, DB expr a, Lambda expr, Lit expr a)
-  => a -> a -> expr e b
-pt1' a b = pt1 $: lit a $: lit b
-
 type Zq q = ZqBasic q Int64
 
 main :: IO ()
 main = do
-  let (exp1a, exp1b) = dup $ pt1 @(Cyc CT F4 (Zq 7), PNoise 'Z (Cyc CT F4 (Zq 7))) @()
-      (exp2a, exp2b) = dup $ pt1' @(Cyc CT F4 (Zq 7), Cyc CT F4 (Zq 7)) @() (7,7) (11,11)
+  -- look Ma, no types!
+  putStrLn $ pprint pt1
+  putStrLn $ show $ eval (pt1 @Int) 7 11
 
-  -- print the unapplied PT function
-  putStrLn $ pprint exp1a
-  --putStrLn $ pprint exp1b
-  -- apply the PT function to arguments, then print it out
-  putStrLn $ pprint exp2a
-  -- apply the PT function to arguments and evaluate the function
-  putStrLn $ show $ eval exp2b
-  putStrLn $ show $ eval (pt1 @(Cyc CT F4 (Zq 7))) 7 11
   -- compile the un-applied function to CT, then print it out
-  (x,_) <- compile
+  (x,st) <- compile
          @'[ '(F4, F8) ]
-         @'[ Zq $(mkQ $ 2^(4 :: Int))] -- , Zq $(mkQ 11) ]
+         @'[ Zq $(mkQ $ 2^(40 :: Int))] -- , Zq $(mkQ 11) ]
          @(Zq $(mkQ 13))
          @TrivGad
          @Double
          1.0
-         exp1b
-  putStrLn $ pprint x
+         (pt1 @(PNoise 'Z (Cyc CT F4 (Zq 7))))
 
+  let (z1,z2) = dup x
+  arg1 <- fromJust $ encryptArg st 7
+  arg2 <- fromJust $ encryptArg st 11
+  putStrLn $ pprint z1
+  putStrLn $ show $ eval z2 arg1 arg2
+
+type B = SHE.CT F4 (Zq 7) (Cyc CT F8 (Zq $(mkQ $ 2^(40 :: Int))))
+type A = B
+type F = (A,A) -> (A,A) -> (B,B)
+{-
 foo ::
   (m'map ~ '[ '(F4, F8) ],
    zqs ~ '[ Zq $(mkQ $ 2^(4 :: Int))],
@@ -77,8 +74,10 @@ foo ::
    a ~ PreMul expr b)
   => expr () (a -> a -> b)
 foo = pt1
+-}
 
 type Zqs = '[ Zq $(mkQ $ 2^(4 :: Int)) ] -- , Zq $(mkQ 11) ]
 
 -- write an interpreter to remove "rescale a -> a"
 -- warnings about moduli mismatches
+-- encapsulation for compile CTs? (CTWrapper?)
