91867428c6a199c50f444b233d32c284d9680c20
Chris Peikert <cpeikert@alum.mit.edu>
2017-04-30 17:15:34 -0400

    consistent constraint and function names, wibble

--- alchemy/Crypto/Alchemy/Language/SHE.hs
+++ alchemy/Crypto/Alchemy/Language/SHE.hs
@@ -13,30 +13,35 @@ import GHC.Exts
 
 class SHE expr where
 
-  type ModSwitchCtx expr ct zp' :: Constraint
-  type RescaleCtx   expr ct zq' :: Constraint
-  type AddPubCtx    expr ct     :: Constraint
-  type MulPubCtx    expr ct     :: Constraint
-  type KeySwitchCtx expr ct zq' gad :: Constraint
-  type TunnelCtx    expr (t :: Factored -> * -> *) (e :: Factored) (r :: Factored) (s :: Factored) (e' :: Factored) (r' :: Factored) (s' :: Factored) zp zq gad :: Constraint
+  type ModSwitchPTCtx   expr ct zp'     :: Constraint
+  type RescaleLinearCtx expr ct zq'     :: Constraint
+  type AddPublicCtx     expr ct         :: Constraint
+  type MulPublicCtx     expr ct         :: Constraint
+  type KeySwitchQuadCtx expr ct zq' gad :: Constraint
+  type TunnelCtx
+    expr
+    (t  :: Factored -> * -> *)
+    (e  :: Factored) (r  :: Factored) (s  :: Factored)
+    (e' :: Factored) (r' :: Factored) (s' :: Factored)
+    zp zq gad :: Constraint
 
-  modSwitchPT :: (ModSwitchCtx expr ct zp', ct ~ CT m zp (Cyc t m' zq))
+  modSwitchPT :: (ModSwitchPTCtx expr ct zp', ct ~ CT m zp (Cyc t m' zq))
     => expr env ct -> expr env (CT m zp' (Cyc t m' zq))
 
-  rescaleLinearCT :: (RescaleCtx expr ct zq', ct ~ CT m zp (Cyc t m' zq))
+  rescaleLinear :: (RescaleLinearCtx expr ct zq', ct ~ CT m zp (Cyc t m' zq))
     => expr env (CT m zp (Cyc t m' zq')) -> expr env ct
 
   -- CJP: doesn't quite fall into addLit, though we could possibly
   -- generalize addLit to cover this (not clear if a good idea; this
   -- signature is pretty special)
-  addPublic :: (AddPubCtx expr ct, ct ~ CT m zp (Cyc t m' zq))
+  addPublic :: (AddPublicCtx expr ct, ct ~ CT m zp (Cyc t m' zq))
     => Cyc t m zp -> expr env ct -> expr env ct
 
   -- CJP: ditto here
-  mulPublic :: (MulPubCtx expr ct, ct ~ CT m zp (Cyc t m' zq))
+  mulPublic :: (MulPublicCtx expr ct, ct ~ CT m zp (Cyc t m' zq))
     => Cyc t m zp -> expr env ct -> expr env ct
 
-  keySwitchQuad :: (KeySwitchCtx expr ct zq' gad, ct ~ CT m zp (Cyc t m' zq))
+  keySwitchQuad :: (KeySwitchQuadCtx expr ct zq' gad, ct ~ CT m zp (Cyc t m' zq))
     => KSQuadCircHint gad (Cyc t m' zq') -> expr env ct -> expr env ct
 
   tunnel :: (TunnelCtx expr t e r s e' r' s' zp zq gad)
