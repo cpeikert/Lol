a891557d6a6660847c583c02cc0c6f66d3cbdd72
Chris Peikert <cpeikert@alum.mit.edu>
2017-05-02 11:17:20 -0400

    Monad methods are named, and entirely in obj lang. Operators are symbols, top-level, and in meta lang.

--- alchemy/Crypto/Alchemy/Language/Monad.hs
+++ alchemy/Crypto/Alchemy/Language/Monad.hs
@@ -1,34 +1,65 @@
 
 module Crypto.Alchemy.Language.Monad where
 
+import Crypto.Alchemy.Language.Lambda
+
 -- | Symantics for functors: promotes any 'Functor' to the object
 -- language. (Instances should obey the monad laws.)
 
-class Functor_ expr where
+class Lambda expr => Functor_ expr where
   -- | Object-language analogue of 'fmap'.
-  fmap_ :: (Functor f) => expr e (a -> b) -> expr e (f a) -> expr e (f b)
+  fmap_ :: (Functor f) => expr e ((a -> b) -> f a -> f b)
 
--- | Infix synonym for 'fmap_'.
+-- | Convenient metalanguage version of 'fmap_'.
+infixl 4 <$:>
 (<$:>) :: (Functor_ expr, Functor f) =>
           expr e (a -> b) -> expr e (f a) -> expr e (f b)
-(<$:>) = fmap_
+f <$:> a = fmap_ $: f $: a
 
 -- | Symantics for applicative monads: promotes any 'Applicative' to
 -- the object language. (Instances should obey the monad laws.)
 
 class (Functor_ expr) => Applicative_ expr where
   -- | Object-language analogue of 'pure'.
-  pure_  :: (Applicative f) => expr e a -> expr e (f a)
+  pure_ :: (Applicative f) => expr e (a -> f a)
 
   -- | Object-language analogue of '(<*>)'.
-  (<*:>) :: (Applicative f) => expr e (f (a -> b)) -> expr e (f a) -> expr e (f b)
+  ap_   :: (Applicative f) => expr e (f (a -> b) -> f a -> f b)
+
+-- | Convenient metalanguage version of 'ap_'.
+infixl 4 <*:>
+(<*:>) :: (Applicative_ expr, Applicative f) =>
+  expr e (f (a -> b)) -> expr e (f a) -> expr e (f b)
+f <*:> a = ap_ $: f $: a
+
+-- | Object-language analogue of 'liftA' (synonym for 'fmap_').
+liftA_ :: (Applicative_ expr, Applicative f) => expr e ((a -> b) -> f a -> f b)
+liftA_ = fmap_
+
+-- | Object-language analogue of 'liftA2' (synonym for 'fmap_').
+liftA2_ :: (Applicative_ expr, Applicative f) =>
+           expr e ((a -> b -> c) -> f a -> f b -> f c)
+-- everything must be written in the object language here because
+-- that's where liftA2_ lives
+liftA2_ = lam $ lam $ lam $ ap_ $: (fmap_ $: v2 $: v1) $: v0
+
+liftA3_ :: (Applicative_ expr, Applicative f) =>
+           expr e ((a -> b -> c -> d) -> f a -> f b -> f c -> f d)
+liftA3_ = lam $ lam $ lam $ lam $
+  ap_ $: (ap_ $: (fmap_ $: v3 $: v2) $: v1) $: v0
 
 -- | Symantics for monads: promotes any 'Monad' to the object
 -- language. (Instances should obey the monad laws.)
 
 class (Applicative_ expr) => Monad_ expr where
   -- | Object-language analogue of '(>>=)'.
-  (>>=:) :: (Monad m) => expr e (m a) -> expr e (a -> m b) -> expr e (m b)
+  bind_ :: (Monad m) => expr e (m a -> (a -> m b) -> m b)
 
-return_ :: (Monad_ expr, Monad m) => expr e a -> expr e (m a)
+-- | Convenient metalanguage version of 'bind_'.
+infixl 1 >>=:
+(>>=:) :: (Monad_ expr, Monad m) =>
+          expr e (m a) -> expr e (a -> m b) -> expr e (m b)
+a >>=: f = bind_ $: a $: f
+
+return_ :: (Monad_ expr, Monad m) => expr e (a -> m a)
 return_ = pure_
