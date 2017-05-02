b056cf5b322dbc1aacfba200806a3518068de386
Chris Peikert <cpeikert@alum.mit.edu>
2017-05-01 23:21:57 -0400

    renamed type to DedupRescale; implemented add/mul

--- alchemy/Crypto/Alchemy/Interpreter/DedupRescale.hs
+++ alchemy/Crypto/Alchemy/Interpreter/DedupRescale.hs
@@ -22,15 +22,15 @@ import Data.Typeable
 -- with the possible exception of rescaleCT itself, if the context is right.
 -- Thus it seems cleaner to work "bottom-up" rather than "top-down".
 
-data DupRescale expr e a where
+data DedupRescale expr e a where
   -- indicates that `expr a` is the result of a rescale from b to a
   -- holds the value both pre- and post-rescaling
-  Ctx :: (Typeable b) => {ctx :: expr e b, dedupRescale :: expr e a} -> DupRescale expr e a
+  Ctx :: (Typeable b) => {ctx :: expr e b, dedupRescale :: expr e a} -> DedupRescale expr e a
   -- indicates that `expr a` is *not* the output of rescaleCT.
-  NoCtx :: {dedupRescale :: expr e a} -> DupRescale expr e a
+  NoCtx :: {dedupRescale :: expr e a} -> DedupRescale expr e a
 
 -- map, ignoring context
-dupMap :: (expr e a -> expr e b) -> DupRescale expr e a -> DupRescale expr e b
+dupMap :: (expr e a -> expr e b) -> DedupRescale expr e a -> DedupRescale expr e b
 dupMap f = NoCtx . f . dedupRescale
 
 
@@ -38,38 +38,38 @@ dupMap f = NoCtx . f . dedupRescale
 -- consider: (\x -> (rescaleDown x) + (x *x)) (rescaleUp y)
 -- if we simplify this to \y -> y + ((rescaleUp y)*(rescaleUp y)),
 -- we end up doing the rescale twice (but remove the duplicate)
-instance (Lambda expr) => Lambda (DupRescale expr) where
+instance (Lambda expr) => Lambda (DedupRescale expr) where
   lam f = NoCtx $ lam $ dedupRescale f
   f $: a = NoCtx $ (dedupRescale f) $: (dedupRescale a)
   v0  = NoCtx v0
   s a = NoCtx $ s $ dedupRescale a
 
-instance (Add expr a) => Add (DupRescale expr) a where
-  a +: b = NoCtx $ (dedupRescale a) +: (dedupRescale b)
-  neg = dupMap neg
+instance (Add expr a) => Add (DedupRescale expr) a where
+  add = NoCtx add
+  neg = NoCtx neg
 
-instance (Mul expr a) => Mul (DupRescale expr) a where
-  type PreMul (DupRescale expr) a = PreMul expr a
-  a *: b = NoCtx $ (dedupRescale a) *: (dedupRescale b)
+instance (Mul expr a) => Mul (DedupRescale expr) a where
+  type PreMul (DedupRescale expr) a = PreMul expr a
+  mul = NoCtx mul
 
-instance (SHE expr) => SHE (DupRescale expr) where
+instance (SHE expr) => SHE (DedupRescale expr) where
 
-  type ModSwitchPTCtx (DupRescale expr) ct zp' =
+  type ModSwitchPTCtx (DedupRescale expr) ct zp' =
     (ModSwitchPTCtx expr ct zp')
-  type RescaleLinearCtx (DupRescale expr) (CT m zp (Cyc t m' zq)) zq' =
+  type RescaleLinearCtx (DedupRescale expr) (CT m zp (Cyc t m' zq)) zq' =
     (Typeable (CT m zp (Cyc t m' zq)),
      Typeable (CT m zp (Cyc t m' zq')),
      RescaleLinearCtx expr (CT m zp (Cyc t m' zq)) zq')
-  type AddPublicCtx (DupRescale expr) ct = (AddPublicCtx expr ct)
-  type MulPublicCtx (DupRescale expr) ct = (MulPublicCtx expr ct)
-  type KeySwitchQuadCtx (DupRescale expr) ct zq' gad = (KeySwitchQuadCtx expr ct zq' gad)
-  type TunnelCtx    (DupRescale expr) t e r s e' r' s' zp zq gad = (TunnelCtx expr t e r s e' r' s' zp zq gad)
+  type AddPublicCtx (DedupRescale expr) ct = (AddPublicCtx expr ct)
+  type MulPublicCtx (DedupRescale expr) ct = (MulPublicCtx expr ct)
+  type KeySwitchQuadCtx (DedupRescale expr) ct zq' gad = (KeySwitchQuadCtx expr ct zq' gad)
+  type TunnelCtx    (DedupRescale expr) t e r s e' r' s' zp zq gad = (TunnelCtx expr t e r s e' r' s' zp zq gad)
 
   modSwitchPT = dupMap modSwitchPT
 
   rescaleLinear :: forall ct zq' m zp t m' zq e .
-    (RescaleLinearCtx (DupRescale expr) (CT m zp (Cyc t m' zq)) zq', ct ~ CT m zp (Cyc t m' zq))
-            => (DupRescale expr) e (CT m zp (Cyc t m' zq')) -> (DupRescale expr) e ct
+    (RescaleLinearCtx (DedupRescale expr) (CT m zp (Cyc t m' zq)) zq', ct ~ CT m zp (Cyc t m' zq))
+            => (DedupRescale expr) e (CT m zp (Cyc t m' zq')) -> (DedupRescale expr) e ct
   rescaleLinear (Ctx (prev :: expr e ct') x) =
     case (eqT :: Maybe (ct' :~: ct)) of
       -- previous op scaled from zq -> zq', so rather than rescaling back down, we just use the un-rescaled value
