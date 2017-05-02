eadc84c9b6c1e0b2bfbcc15cec3fab6bcfc9326a
Chris Peikert <cpeikert@alum.mit.edu>
2017-05-01 22:26:50 -0400

    merged DB into Lambda; it doesn't need an argument

--- alchemy/Crypto/Alchemy/Language/Lambda.hs
+++ alchemy/Crypto/Alchemy/Language/Lambda.hs
@@ -12,37 +12,33 @@ class Lambda expr where
   -- | Application.
   ($:) :: expr e (a -> b) -> expr e a -> expr e b
 
+  -- | The zero'th (most recently bound) variable.
+  v0 :: expr (b,a) a
+
+  -- | Extend environment.
+  s  :: expr e a -> expr (e,x) a
+
 -- | Let-sharing.
 let_ :: Lambda expr => expr e a -> expr (e,a) b -> expr e b
 let_ a f = lam f $: a
 
 -- | Composition.
 infixr 9 .:
--- could also drop the DB (a->b), (b->c) and set f,g envs to (e,a)
-(.:) :: (Lambda expr, DB expr a, DB expr (a -> b), DB expr (b -> c)) =>
-        expr e (b -> c) -> expr e (a -> b) -> expr e (a -> c)
+(.:) :: (Lambda expr) => expr e (b -> c) -> expr e (a -> b) -> expr e (a -> c)
 f .: g = lam (s f $: (s g $: v0))
 
--- | Symantics for de Bruijn variables.
-
-class DB expr a where
-  -- | The zero'th (most recently bound) variable.
-  v0 :: expr (b,a) a
-
-  -- | The next-most-recently-bound variable from the given one.
-  s  :: expr e a -> expr (e,x) a
 
 -- CJP: for some reason have to give signature here, even though ghci
 -- infers them correctly
 
-v1 :: DB expr b => expr ((c,b),a) b
+v1 :: Lambda expr => expr ((c,b),a) b
 v1 = s v0
 
-v2 :: DB expr c => expr (((d,c),b),a) c
+v2 :: Lambda expr => expr (((d,c),b),a) c
 v2 = s v1
 
-v3 :: DB expr d => expr ((((e,d),c),b),a) d
+v3 :: Lambda expr => expr ((((e,d),c),b),a) d
 v3 = s v2
 
-v4 :: DB expr e => expr (((((f,e),d),c),b),a) e
+v4 :: Lambda expr => expr (((((f,e),d),c),b),a) e
 v4 = s v3
