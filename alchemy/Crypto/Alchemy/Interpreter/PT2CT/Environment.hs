be00d9137d5e43bf0772348187d8dac31a5f7449
Eric <ecrockett0@gmail.com>
2017-05-03 20:38:54 -0400

    Achieved simulation of MR superclass constraint using 'embedReader'

--- alchemy/Crypto/Alchemy/Interpreter/PT2CT/Environment.hs
+++ alchemy/Crypto/Alchemy/Interpreter/PT2CT/Environment.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE LambdaCase          #-}
 {-# LANGUAGE RecordWildCards     #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -13,8 +14,7 @@ module Crypto.Alchemy.Interpreter.PT2CT.Environment
 , newP2CState
 , tunnelHint
 , getKSHint
-, lookupKey
-, lookupHint
+, lookup'
 , (>=<))
 where
 
@@ -32,21 +32,31 @@ import Crypto.Lol.Applications.SymmSHE
 ---- Monad helper functions
 
 -- | Holds keys and hints generated during the compilation process.
-data P2CState = St { keys :: [Dynamic], hints :: [Dynamic] } deriving (Show)
+newtype P2CState = St {unST :: [Dynamic]} deriving (Show, Monoid)
 
 newP2CState :: P2CState
-newP2CState = St [] []
+newP2CState = St []
 
 -- retrieve a key from the state, or generate one if it doesn't exist
 getKey :: forall z v mon t m' . (MonadReader v mon, MonadAccumulator P2CState mon,
            MonadRandom mon, GenSKCtx t m' z v, Typeable (Cyc t m' z))
   => mon (SK (Cyc t m' z))
-getKey = lookupKey >>= \case
+getKey = embedReader lookup' >>= \case
   (Just t) -> return t
   -- generate a key with the variance stored in the Reader monad
   Nothing -> do
     v <- ask
-    append =<< genSK (v :: v)
+    put' >=< genSK (v :: v)
+
+put' :: (MonadAccumulator P2CState m, Typeable a) => a -> m ()
+put' sk = append $ St [toDyn sk] -- modify' $ \St{..} -> St {keys=toDyn sk : keys,..}
+
+(>=<) :: (Monad m) => (a -> m ()) -> m a -> m a
+a >=< b = do
+  b' <- b
+  a b'
+  return b'
+
 
 -- not memoized right now, but could be if we also store the linear function as part of the lookup key
 -- EAC: https://ghc.haskell.org/trac/ghc/ticket/13490
@@ -67,22 +77,19 @@ getKSHint :: forall v mon t z gad m' zq zq' ksmod .
   (-- constraints for getKey
    MonadReader v mon, MonadAccumulator P2CState mon,
    MonadRandom mon, GenSKCtx t m' z v, Typeable (Cyc t m' z),
-   -- constraints for lookupHint
+   -- constraints for lookup
    Typeable (KSQuadCircHint gad (Cyc t m' zq')),
    -- constraints for ksQuadCircHint
    KSHintCtx gad t m' z zq', zq' ~ (ksmod, zq)) -- EAC: Note that order matches the optimized RescaleCyc instance
   => Proxy ksmod -> Proxy z -> Proxy zq -> mon (KSQuadCircHint gad (Cyc t m' zq'))
-getKSHint _ _ _ = lookupHint >>= \case
+getKSHint _ _ _ = embedReader lookup' >>= \case
   (Just h) -> return h
   Nothing -> do
     sk :: SK (Cyc t m' z) <- getKey
-    append =<< ksQuadCircHint sk
+    put' >=< ksQuadCircHint sk
 
-lookupKey :: (Typeable a, MonadReader P2CState mon) => mon (Maybe a)
-lookupKey = (dynLookup . keys) <$> ask
-
-lookupHint :: (Typeable a, MonadReader P2CState mon) => mon (Maybe a)
-lookupHint = (dynLookup . hints) <$> ask
+lookup' :: (Typeable a, MonadReader P2CState mon) => mon (Maybe a)
+lookup' = (dynLookup . unST) <$> ask
 
 -- lookup an item in a list of dynamics
 dynLookup :: (Typeable a) => [Dynamic] -> Maybe a
--- alchemy/Crypto/Alchemy/MonadAccumulator.hs
+++ alchemy/Crypto/Alchemy/MonadAccumulator.hs
@@ -10,7 +10,7 @@ import Control.Monad.State
 
 -- | An append-only state monad.
 
-class (MonadReader w m, Monoid w) => MonadAccumulator w m | m -> w where
+class (Monoid w) => MonadAccumulator w m | m -> w where
   -- | Append the given value to the state inside the monad.
   append :: w -> m ()
 
@@ -19,9 +19,12 @@ class (MonadReader w m, Monoid w) => MonadAccumulator w m | m -> w where
   -- the previous state.
   accumulate  :: (w -> (a,w)) -> m a
 
-instance (MonadState w m, MonadReader w m, Monoid w) => MonadAccumulator w m where
+instance (MonadState w m, Monoid w) => MonadAccumulator w m where
   append w = modify (`mappend` w)
   accumulate f = state $ \w -> let (a,z) = f w in (a, w `mappend` z)
 
 runAccumulator :: (Monoid w) => State w a -> (a, w)
 runAccumulator = flip runState mempty
+
+embedReader :: (MonadAccumulator s m) => Reader s a -> m a
+embedReader x = accumulate $ \s -> (runReader x s, s)
