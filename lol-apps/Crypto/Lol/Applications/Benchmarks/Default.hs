5eccdfeecba4beec75d43afb31ea91a92cd7733c
Chris Peikert <cpeikert@alum.mit.edu>
2017-05-05 21:28:17 -0400

    email typo

--- lol-apps/Crypto/Lol/Applications/Benchmarks/Default.hs
+++ lol-apps/Crypto/Lol/Applications/Benchmarks/Default.hs
@@ -4,7 +4,7 @@ Description : Default benchmarks for lol-apps.
 Copyright   : (c) Eric Crockett, 2011-2017
                   Chris Peikert, 2011-2017
 License     : GPL-3
-Maintainer  : ecrockett0@email.com
+Maintainer  : ecrockett0@gmail.com
 Stability   : experimental
 Portability : POSIX
 
