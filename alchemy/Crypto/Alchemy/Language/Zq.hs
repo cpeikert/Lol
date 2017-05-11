7246308260d45904cfd3d62ea8c1720a232a3cda
Chris Peikert <cpeikert@alum.mit.edu>
2017-05-11 19:19:11 -0400

    renamed module ZqPow2 -> Zq, methods

--- alchemy/Crypto/Alchemy/Language/Zq.hs
+++ alchemy/Crypto/Alchemy/Language/Zq.hs
@@ -9,26 +9,23 @@
 
 {-# LANGUAGE DataKinds #-}
 
-module Crypto.Alchemy.Language.ZqPow2 where
+module Crypto.Alchemy.Language.Zq where
 
 import Crypto.Lol       (Pos (..), Prime2, PrimePower (PP))
 import Crypto.Lol.Types (ZqBasic)
 
 type Pow2 e = 'PP '(Prime2, e)
 
--- | Symantics for division-by-2 of argument and modulus.
+-- | Symantics for division-by-2 of both the value and its modulus.
 
-class DivZq expr a where
+class Div2Zq expr a where
   type family PreDiv expr a
 
-  divZq :: expr (PreDiv expr a -> a)
+  div2Zq_ :: expr (PreDiv expr a -> a)
 
 
 -- | Symantics for rescaling on a power-of-two modulus.
 
 class RescaleZqPow2 expr where
   -- | Rescale (round) the argument from \( \Z_{2^e} \) to \( \Z_2 \).
-  rescaleZqPow2 :: expr (ZqBasic (Pow2 e) i -> ZqBasic (Pow2 'O) i)
-
-
--- mapCRTSlots (rescaleTree rescaleZqPow2) :: (Arithmetic expr) => expr (cyc8 -> cyc2)
+  rescaleZqPow2_ :: expr (ZqBasic (Pow2 e) i -> ZqBasic (Pow2 'O) i)
