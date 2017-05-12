5dafd74eeaa5fcdd98aa5652837d90a111a43a69
Eric <ecrockett0@gmail.com>
2017-05-12 15:40:49 -0400

    Added RescaleZqPow2 instance for PNoise in RescaleToTree

--- alchemy/Crypto/Alchemy/Interpreter/RescaleToTree.hs
+++ alchemy/Crypto/Alchemy/Interpreter/RescaleToTree.hs
@@ -1,13 +1,14 @@
-{-# LANGUAGE DataKinds            #-}
-{-# LANGUAGE FlexibleContexts       #-}
-{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE ConstraintKinds            #-}
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# LANGUAGE RebindableSyntax      #-}
-{-# LANGUAGE ScopedTypeVariables   #-}
-{-# LANGUAGE TypeFamilies          #-}
-{-# LANGUAGE UndecidableInstances  #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE PartialTypeSignatures      #-}
+{-# LANGUAGE RebindableSyntax           #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE UndecidableInstances       #-}
 
 {-# OPTIONS_GHC -fno-warn-partial-type-signatures #-}
 
@@ -98,24 +99,46 @@ instance (Lambda expr) => RescaleZqPow2 (RescaleToTree expr) 'O (PNoise h (ZqBas
   -- k = 1, so p = 2^k = 2
   rescaleZqPow2_ = pure $ RT $ lam v0
 
-instance (z2k ~ PreRescale expr k (ZqBasic PP2 i), prediv ~ PreDiv2 expr z2k,
-  Lambda expr, Internal expr k (ZqBasic PP2 i), Reflects ('S k) Int,
-  Mul expr prediv, Div2 expr z2k,
-  Ring (PreMul expr prediv), Ring prediv,
-  AddLit expr (PreMul expr prediv), AddLit expr prediv,
-  ToInteger i)
+instance (RescaleToTreeCtx expr k (ZqBasic PP2 i))
   => RescaleZqPow2 (RescaleToTree expr) ('S k) (ZqBasic PP2 i) where
 
   type PreRescaleZqPow2 (RescaleToTree expr) ('S k) (ZqBasic PP2 i) =
     PreRescale expr ('S k) (ZqBasic PP2 i)
 
-  -- k > 1, so p = 2^k >= 4, meaning we can divide p by 4
-  rescaleZqPow2_ = pure $ RT $ lam $
+  rescaleZqPow2_ = rescaleZqPow2__
+
+instance (RescaleToTreeCtx expr k (PNoise h (ZqBasic PP2 i)))
+  => RescaleZqPow2 (RescaleToTree expr) ('S k) (PNoise h (ZqBasic PP2 i)) where
+
+  type PreRescaleZqPow2 (RescaleToTree expr) ('S k) (PNoise h (ZqBasic PP2 i)) =
+    PreRescale expr ('S k) (PNoise h (ZqBasic PP2 i))
+
+  rescaleZqPow2_ = rescaleZqPow2__
+
+type RescaleToTreeCtx expr k z2 =
+  RescaleToTreeCtx' expr k z2 (PreRescale expr k z2)
+
+type RescaleToTreeCtx' expr k z2 z2k =
+  (Div2 expr z2k, RescaleToTreeCtx'' expr k z2 (PreDiv2 expr z2k))
+
+type RescaleToTreeCtx'' expr k z2 prediv =
+  (Lambda expr, AddLit expr prediv,
+   PreRescaleZqPow2 (RescaleToTree expr) ('S k) z2 ~ PreMul expr prediv,
+   Internal expr k z2, Reflects ('S k) Int, Mul expr prediv,
+   Ring (PreMul expr prediv), Ring prediv, AddLit expr (PreMul expr prediv))
+
+-- k > 1, so p = 2^k >= 4, meaning we can divide p by 4
+rescaleZqPow2__ :: forall expr k z2 e .
+  (RescaleToTreeCtx expr k z2)
+  => Tagged ('S k) (RescaleToTree expr e (PreRescaleZqPow2 (RescaleToTree expr) ('S k) z2 -> z2))
+rescaleZqPow2__ = pure $ RT $ lam $
     let xprod = v0 *: (one >+: v0)
         lgpVal = proxy value (Proxy::Proxy ('S k)) :: Int
         pDiv4 = 2^(lgpVal-2)
     in internal (Proxy::Proxy k) $ take pDiv4 $
          map ((div2_ $:) . (>+: xprod)) [fromInteger $ y * (-y+1) | y <- [1..]]
+
+
 {-
 instance (z2k ~ PreRescale expr k (ZqBasic PP2 i), prediv ~ PreDiv2 expr z2k,
   Lambda expr, Internal expr k (ZqBasic PP2 i), Reflects ('S k) Int,
