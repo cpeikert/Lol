575e8bfc8dddbc75fad2729674a5ae96db76b775
Eric <ecrockett0@gmail.com>
2017-05-12 11:18:56 -0400

    Got RescaleToTree instances for ZqBasic, still need for PNoise

--- alchemy/Crypto/Alchemy/Language/RescaleZqPow2.hs
+++ alchemy/Crypto/Alchemy/Language/RescaleZqPow2.hs
@@ -1,7 +1,6 @@
-{-# LANGUAGE DataKinds              #-}
-{-# LANGUAGE MultiParamTypeClasses  #-}
-{-# LANGUAGE TypeFamilies           #-}
-{-# LANGUAGE TypeFamilyDependencies #-}
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies          #-}
 
 {-|
 
@@ -11,14 +10,13 @@
 
 module Crypto.Alchemy.Language.RescaleZqPow2 where
 
-import Crypto.Lol (Pos)
+import Crypto.Lol (Pos, Tagged)
 
 -- | Symantics for rescaling the integers modulo a power of two.
 
 class RescaleZqPow2 expr (k::Pos) z2 where
-  -- | The type corresponding to \( \Z_{2^k} \).  (The type should
-  -- determine the exponent, hence the partially injectivity.)
-  type PreRescaleZqPow2 expr (k::Pos) z2 = z2k | z2k -> k
+
+  type PreRescaleZqPow2 expr (k::Pos) z2
 
   -- | Rescale (round) the argument from \( \Z_{2^k} \) to \( \Z_2 \).
-  rescaleZqPow2_ :: expr e (PreRescaleZqPow2 expr k z2 -> z2)
+  rescaleZqPow2_ :: Tagged k (expr e (PreRescaleZqPow2 expr k z2 -> z2))
