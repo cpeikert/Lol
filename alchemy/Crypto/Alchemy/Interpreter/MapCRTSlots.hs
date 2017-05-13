f5ac456c9c29ac589e898772f48a8597104997e7
Eric <ecrockett0@gmail.com>
2017-05-13 00:13:43 -0400

    Added cases for Zq2Cyc/Cyc2Zq, removed injectivity for t,m because () isn't injective in them. Fixed overlapping instances: instance heads must be fully expanded to avoid overlap.

--- alchemy/Crypto/Alchemy/Interpreter/MapCRTSlots.hs
+++ alchemy/Crypto/Alchemy/Interpreter/MapCRTSlots.hs
@@ -15,11 +15,15 @@ import Crypto.Alchemy.Interpreter.PT2CT.Noise
 import Crypto.Lol
 import Crypto.Lol.Types (ZqBasic)
 
-type family Zq2Cyc t m a = cyca | cyca -> a t m where
+import Control.Monad.Identity
+
+type family Zq2Cyc t m a = cyca | cyca -> a where
   Zq2Cyc t m (ZqBasic (q :: k) i) = Cyc t m (ZqBasic q i)
   Zq2Cyc t m (a -> b) = Zq2Cyc t m a -> Zq2Cyc t m b
   Zq2Cyc t m (a, b) = (Zq2Cyc t m a, Zq2Cyc t m b)
   Zq2Cyc t m (PNoise h a) = PNoise h (Zq2Cyc t m a)
+  Zq2Cyc t m (Identity a) = Identity (Zq2Cyc t m a)
+  Zq2Cyc t m () = ()
   -- CJP: TODO include an error case to make things break when they should
 
 -- inverse of the above
@@ -28,6 +32,7 @@ type family Cyc2Zq cyc where
   Cyc2Zq (a -> b) = Cyc2Zq a -> Cyc2Zq b
   Cyc2Zq (a, b) = (Cyc2Zq a, Cyc2Zq b)
   Cyc2Zq (PNoise h a) = PNoise h (Cyc2Zq a)
+  Cyc2Zq (Identity a) = Identity (Cyc2Zq a)
   -- CJP: TODO error here
 
 newtype MapCRTSlots expr t m e a =
@@ -35,14 +40,6 @@ newtype MapCRTSlots expr t m e a =
 
 mapCRTSlots :: MapCRTSlots expr t m e a -> expr (Zq2Cyc t m e) (Zq2Cyc t m a)
 mapCRTSlots = unM
-
-instance (Div2 expr (Zq2Cyc t m a), rq' ~ (PreDiv2 expr (Zq2Cyc t m a)),
-          -- Zq2Cyc, Cyc2Zq must be inverses in this scenario
-          Zq2Cyc t m (Cyc2Zq rq') ~ rq') => Div2 (MapCRTSlots expr t m) a where
-
-  type PreDiv2 (MapCRTSlots expr t m) a = Cyc2Zq (PreDiv2 expr (Zq2Cyc t m a))
-
-  div2_ = M div2_
 
 instance Lambda expr => Lambda (MapCRTSlots expr t m) where
   lam    = M . lam . unM
@@ -64,12 +61,20 @@ instance (Mul expr (Zq2Cyc t m a),
 
   mul_ = M mul_
 
-instance (zq ~ ZqBasic q i, AddLit expr (Cyc t m zq)) =>
-  AddLit (MapCRTSlots expr t m) zq where
+instance (Div2 expr (Zq2Cyc t m a), rq' ~ (PreDiv2 expr (Zq2Cyc t m a)),
+          -- Zq2Cyc, Cyc2Zq must be inverses in this scenario
+          Zq2Cyc t m (Cyc2Zq rq') ~ rq') => Div2 (MapCRTSlots expr t m) a where
+
+  type PreDiv2 (MapCRTSlots expr t m) a = Cyc2Zq (PreDiv2 expr (Zq2Cyc t m a))
+
+  div2_ = M div2_
+
+instance (AddLit expr (Cyc t m (ZqBasic q i))) =>
+  AddLit (MapCRTSlots expr t m) (ZqBasic q i) where
   addLit_ a = M $ addLit_ (scalarCyc a)
 
-instance (zq ~ ZqBasic q i, MulLit expr (Cyc t m zq)) =>
-  MulLit (MapCRTSlots expr t m) zq where
+instance (MulLit expr (Cyc t m (ZqBasic q i))) =>
+  MulLit (MapCRTSlots expr t m) (ZqBasic q i) where
   mulLit_ a = M $ mulLit_ (scalarCyc a)
 
 instance (Functor f, Zq2Cyc t m (f zq) ~ f (Cyc t m zq),
@@ -81,6 +86,7 @@ instance (Functor f, Zq2Cyc t m (f zq) ~ f (Cyc t m zq),
           MulLit expr (f (Cyc t m zq))) =>
   MulLit (MapCRTSlots expr t m) (f zq) where
   mulLit_ a = M $ mulLit_ (scalarCyc <$> a)
+
 
 -- CJP: unclear whether we can instantiate Functor_; should Zq2Cyc be
 -- applied to the functor itself?  For functor (ZqB ->) it would seem
