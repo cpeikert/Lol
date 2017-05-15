dbe8a8101d3e9177327379229fb971644989d0b1
Chris Peikert <cpeikert@alum.mit.edu>
2017-05-14 22:23:59 -0400

    radical simplification of rescaleTreePow2

--- alchemy/Crypto/Alchemy/Interpreter/RescaleTree.hs
+++ alchemy/Crypto/Alchemy/Interpreter/RescaleTree.hs
@@ -14,16 +14,12 @@
 {-# OPTIONS_GHC -fno-warn-partial-type-signatures #-}
 
 {-|
-
   \( \def\Z{\mathbb{Z}} \)
-
 -}
 
 module Crypto.Alchemy.Interpreter.RescaleTree
-( rescaleTreeZqPow2_, rescaleTreeCRT_, rescaleTreeCRT, RescaleCycCRTCtx)
+( rescaleTreePow2_, RescaleTreePow2Ctx )
 where
-
-import Crypto.Alchemy.Interpreter.MapCRTSlots
 
 import Crypto.Alchemy.Language.Arithmetic
 import Crypto.Alchemy.Language.Lambda
@@ -32,83 +28,54 @@ import Crypto.Lol
 -- EAC: shouldn't have to import this
 -- CJP: needed for the Reflects instance for Pos and use of 'value', right?
 import Crypto.Lol.Reflects
-import Crypto.Lol.Types
 
-import Control.Applicative
+type RescaleTreePow2Ctx expr k r2 =
+  (Lambda expr, Reflects k Int, TreeMul expr k r2,
+   Div2 expr (PreRescaleTreePow2 expr k r2),
+   RescaleTreePow2Ctx' expr (PreDiv2 expr (PreRescaleTreePow2 expr k r2)))
 
--- | Constraint synonym for rescaling the CRT slots of a 'Cyc'.
-type RescaleTreeCRTCtx env t m expr k z2 cyc2 cyc2k =
-  (RescaleTreeCRTCtx' expr t m k z2 cyc2 cyc2k,
-   Lambda expr)
--- CJP: left out Weaken until we can resolve other type errors
+type RescaleTreePow2Ctx' expr r2k1 =
+  (AddLit expr r2k1, AddLit expr (PreMul expr r2k1), Mul expr r2k1,
+   Ring r2k1, Ring (PreMul expr r2k1))
 
+type family PreRescaleTreePow2 expr k r2 where
+  PreRescaleTreePow2 expr 'O     r2 = r2
+  PreRescaleTreePow2 expr ('S k) r2 =
+    PreMul expr (PreDiv2 expr (PreRescaleTreePow2 expr k r2))
 
-type RescaleTreeCRTCtx' expr t m k z2 cyc2 cyc2k =
-  (cyc2k ~ Zq2Cyc t m (PreRescaleTreeZqPow2 expr k z2), cyc2 ~ Zq2Cyc t m z2)
+-- | For \( k \geq 1 \), the "rescaling tree" that rounds a
+-- mod-@2^{k+1}@ value to a mod-@2@ value, over the same ring.  This
+-- also works in a SIMD fashion over CRT slots, if all the
+-- mod-@2^{k+1}@ CRT slots hold \( \Z_{2^{k+1}} \) values (otherwise,
+-- the behavior is undefined).
 
--- | Convenience function: using the (arithmetic) rescaling tree,
--- rescale each \( \Z_{2^k} \) value in the mod-\( 2^k \) CRT slots of
--- \( R_{2^k} \) down to \( \Z_2 \), with the result in \( R_2 \).
--- (If the values in the slots are not restricted to \( \Z_{2^k} \),
--- the behavior is undefined.)
-rescaleTreeCRT_ :: forall e t m k z2 cyc2 cyc2k expr .
-  (RescaleTreeCRTCtx e t m expr k z2 cyc2 cyc2k) =>
-  -- CJP: specialized to empty env until we can resolve other type errors
-  Tagged '(t,m,k) (expr () (_ -> _)) 
-  -- Zq2Cyc t m (PreRescaleTreeZqPow2 expr k z2) -> Zq2Cyc t m z2))
-rescaleTreeCRT_ =  pure $
-  mapCRTSlots @t @m $
-  proxy (rescaleTreeZqPow2_ :: Tagged k ((MapCRTSlots expr t m) () (PreRescaleTreeZqPow2 expr k z2 -> z2)))
-  (Proxy::Proxy k)
-
-rescaleTreeCRT :: (RescaleTreeCRTCtx e t m expr k z2 cyc2 cyc2k)
-  => Tagged '(t,m,k) (expr () cyc2k -> expr () (Zq2Cyc t m z2))
-rescaleTreeCRT = ($:) <$> rescaleTreeCRT_
-
-
-type RescaleTreeCtx expr k z2 =
-  RescaleTreeCtx' expr k z2 (PreRescaleTreeZqPow2 expr k z2)
-
-type RescaleTreeCtx' expr k z2 z2k =
-  (Div2 expr z2k, RescaleTreeCtx'' expr k z2 (PreDiv2 expr z2k))
-
-type RescaleTreeCtx'' expr k z2 prediv =
-  (Lambda expr, AddLit expr prediv,
-   Internal expr k z2, Reflects k Int, Mul expr prediv,
-   Ring (PreMul expr prediv), Ring prediv, AddLit expr (PreMul expr prediv))
-
-type family PreRescaleTreeZqPow2 expr k z2 where
-  PreRescaleTreeZqPow2 expr 'O     z2 = z2
-  PreRescaleTreeZqPow2 expr ('S k) z2 =
-    PreMul expr (PreDiv2 expr (PreRescaleTreeZqPow2 expr k z2))
-
-rescaleTreeZqPow2_ :: forall k z2 expr e .
-  -- CJP: check for fencepost errors on k in the constraints
-  (RescaleTreeCtx expr k z2)
-  => Tagged ('S k) (expr e (PreRescaleTreeZqPow2 expr ('S k) z2 -> z2))
-rescaleTreeZqPow2_ = pure $ lam $
+rescaleTreePow2_ :: forall k r2 expr e . (RescaleTreePow2Ctx expr k r2)
+  => Tagged ('S k) (expr e (PreRescaleTreePow2 expr ('S k) r2 -> r2))
+rescaleTreePow2_ = tag $ lam $
     let v'    = v0 *: (one >+: v0)
         kval  = proxy value (Proxy::Proxy k) :: Int
         pDiv4 = 2^(kval-1)
-    in internal (Proxy::Proxy k) $ take pDiv4 $
+    in treeMul (Proxy::Proxy k) $ take pDiv4 $
          map ((div2_ $:) . (>+: v')) [fromInteger $ y * (-y+1) | y <- [1..]]
 
-class Internal expr (k :: Pos) z2 where
-  internal :: Proxy k
-           -> [expr env (PreRescaleTreeZqPow2 expr k z2)]
-           -> expr env z2
+class TreeMul expr (k :: Pos) r2 where
+  treeMul :: Proxy k
+          -> [expr env (PreRescaleTreePow2 expr k r2)]
+          -> expr env r2
 
-instance Internal expr 'O z2 where
-  internal _ [x] = x
-  internal _ _   = error "Internal error in RescaleTreeZqPow2 (internal) base case."
+instance TreeMul expr 'O r2 where
+  treeMul _ [x] = x
+  treeMul _ _   = error "Internal error in TreeMul base case."
 
-instance (Internal expr k z2, Lambda expr,
-          Div2 expr (PreRescaleTreeZqPow2 expr k z2),
-          Mul expr (PreDiv2 expr (PreRescaleTreeZqPow2 expr k z2)))
-  => Internal expr ('S k) z2 where
-  internal _ = internal (Proxy::Proxy k) . map ((div2_ $:) . uncurry (*:)) . listToPairs
+instance (TreeMul expr k r2, Lambda expr,
+          Div2 expr (PreRescaleTreePow2 expr k r2),
+          Mul expr (PreDiv2 expr (PreRescaleTreePow2 expr k r2)))
+  => TreeMul expr ('S k) r2 where
 
-listToPairs :: [a] -> [(a,a)]
-listToPairs []       = []
-listToPairs (a:b:xs) = (a,b) : listToPairs xs
-listToPairs _        = error "listToPairs internal error: odd number of elements"
+  treeMul _ = treeMul (Proxy::Proxy k) .
+              map ((div2_ $:) . uncurry (*:)) . pairs
+
+pairs :: [a] -> [(a,a)]
+pairs []       = []
+pairs (a:b:xs) = (a,b) : pairs xs
+pairs _        = error "pairs internal error: odd number of elements"
