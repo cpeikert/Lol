d4037da3c010dc70545007223e1b8fb73a215feb
Eric <ecrockett0@gmail.com>
2017-05-15 17:50:39 -0400

    Added Depth interpreter

---
+++ alchemy/Crypto/Alchemy/Interpreter/Depth.hs
@@ -0,0 +1,83 @@
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies          #-}
+
+module Crypto.Alchemy.Interpreter.Depth
+( D, depth )
+where
+
+import Crypto.Alchemy.Language.Arithmetic
+import Crypto.Alchemy.Language.Lambda
+import Crypto.Alchemy.Language.List
+import Crypto.Alchemy.Language.Monad
+import Crypto.Alchemy.Language.SHE
+import Crypto.Alchemy.Language.TunnelCyc
+
+newtype D expr a = D { depth :: Int }
+
+instance Add D a where
+  add_ = D 0
+  neg_ = D 0
+
+instance AddLit D a where
+  addLit_ _ = D 0
+
+instance Mul D a where
+  type PreMul D a = a
+  mul_ = D 1
+
+instance MulLit D a where
+  mulLit_ _ = D 0
+
+instance Div2 D a where
+  type PreDiv2 D a = a
+  div2_ = D 0
+
+instance Lambda D where
+  lam (D i) = D i
+  (D f) $: (D a) = D $ f + a
+  v0 = D 0
+  s (D i) = D i
+
+instance List D where
+  nil_ = D 0
+  cons_ = D 0
+
+instance Functor_ D where
+  fmap_ = D 0
+
+instance Applicative_ D where
+  pure_ = D 0
+  ap_ = D 0
+
+instance Monad_ D where
+  bind_ = D 0
+
+instance MonadReader_ D where
+  ask_ = D 0
+  local_ = D 0
+
+instance MonadWriter_ D where
+  tell_ = D 0
+  listen_ = D 0
+
+instance SHE D where
+  type ModSwitchPTCtx   D ct zp' = ()
+  type ModSwitchCtx     D ct zq' = ()
+  type AddPublicCtx     D ct     = ()
+  type MulPublicCtx     D ct     = ()
+  type KeySwitchQuadCtx D ct gad = ()
+  type TunnelCtx D t e r s e' r' s' zp zq gad = ()
+
+  modSwitchPT_ = D 0
+  modSwitch_ = D 0
+  addPublic_ _ = D 0
+  mulPublic_ _ = D 0
+  keySwitchQuad_ _ = D 0
+  tunnel_ _ = D 0
+
+instance TunnelCyc D rep where
+  type PreTunnelCyc D rep = rep
+  type TunnelCycCtx D rep t e r s zp = ()
+
+  tunnelCyc_ _ = D 0
--- alchemy/examples/Arithmetic.hs
+++ alchemy/examples/Arithmetic.hs
@@ -14,14 +14,16 @@ import Control.Monad.Reader
 import Control.Monad.Writer
 
 import Crypto.Alchemy.MonadAccumulator
-import Crypto.Alchemy.Interpreter.PT2CT
-import Crypto.Alchemy.Interpreter.KeysHints
-import Crypto.Alchemy.Interpreter.PT2CT.Noise
 --import Crypto.Alchemy.Interpreter.DedupRescale
+import Crypto.Alchemy.Interpreter.Depth
 import Crypto.Alchemy.Interpreter.Dup
 import Crypto.Alchemy.Interpreter.ErrorRateWriter
 import Crypto.Alchemy.Interpreter.Eval
+import Crypto.Alchemy.Interpreter.KeysHints
 import Crypto.Alchemy.Interpreter.Print
+import Crypto.Alchemy.Interpreter.PT2CT
+import Crypto.Alchemy.Interpreter.PT2CT.Noise
+import Crypto.Alchemy.Interpreter.Size
 
 import Crypto.Alchemy.Language.Arithmetic
 import Crypto.Alchemy.Language.Lambda
@@ -30,6 +32,7 @@ import Crypto.Lol                       hiding (Pos (..))
 import Crypto.Lol.Cyclotomic.Tensor.CPP
 import Crypto.Lol.Types
 
+import Control.Applicative
 import Data.Maybe
 import Data.Type.Natural (Nat (Z))
 
@@ -49,9 +52,12 @@ argToReader f a = flip f a =<< ask
 main :: IO ()
 main = do
   -- no types needed to show a function!
-  putStrLn $ pprint pt1
+  putStrLn $ "PT expression: " ++ pprint pt1
+  putStrLn $ "PT expression size: " ++ (show $ size pt1)
+  putStrLn $ "Expression depth: " ++ (show $ depth pt1)
   -- evaluate a DSL function to a Haskell function, then apply to arguments
-  putStrLn $ show $ eval (pt1 @Int) 7 11
+  let ptresult = eval (pt1 @(Zq 7)) 7 11
+  putStrLn $ "PT evaluation result: " ++ show ptresult
 
   evalKeysHints (0.01 :: Double) $ do
 
@@ -71,21 +77,21 @@ main = do
     arg1 <- argToReader encrypt 7
     arg2 <- argToReader encrypt 11
     -- print the compiled function
-    liftIO $ putStrLn $ pprint w1
-    -- if the first modulus is large enough, this will remove the rescales!
-    liftIO $ putStrLn $ pprint $ w2 -- dedupRescale w2
-    -- evaluate the compiled function, then apply it to encrypted inputs
-    --let result = eval z2 arg1 arg2
+    liftIO $ putStrLn $ "CT expression: " ++ pprint w1
+    liftIO $ putStrLn $ "CT expression size: " ++ (show $ size w2)
 
     z2' <- readerToAccumulator $ writeErrorRates @_ @Int64 z2
     let (result,errors) = runWriter $ eval z2' (return arg1) (return arg2)
-    liftIO $ print errors
+    liftIO $ print $ "Error rates: " ++ show errors
 
     -- show the encrypted result
-    liftIO $ putStrLn $ show result
+    --liftIO $ putStrLn $ "Encrypted evaluation result: " ++ show result
     -- show the decrypted result
-    decResult <- readerToAccumulator $ decrypt result
-    liftIO $ putStrLn $ show $ advisePow $ fromJust decResult
+    decResult <- fromJust <$> (readerToAccumulator $ decrypt result)
+    liftIO $ putStrLn $ "Decrypted evaluation result: " ++ show decResult
+
+    liftIO $ putStrLn $ if decResult == scalarCyc ptresult then "PASS" else "FAIL"
+
 
 -- EAC: TODO
 -- encapsulation for compile CTs? (CTWrapper?)
