7e5cc52b09aea3080643e952b0a672add94ba19e
Chris Peikert <cpeikert@alum.mit.edu>
2017-05-19 08:08:44 -0400

    some move about

--- alchemy/examples/HomomRLWR.hs
+++ alchemy/examples/HomomRLWR.hs
@@ -51,10 +51,20 @@ import Data.Type.Natural hiding (Nat(S))
 
 
 
-type K = P2
+type K = P4
 type Gad = TrivGad
 type RescaleM'Map = '[ '(H5,H5)]
-type RescaleZqs = '[Zq1,Zq2,Zq3,Zq4]
+
+type Zq1 = Zq $(mkTLNatNat 1520064001) -- last mul: > 2^30.5
+type Zq2 = Zq $(mkTLNatNat 3144961)    -- 3 rounding muls: > 2^19
+type Zq3 = Zq $(mkTLNatNat 5241601)
+type Zq4 = Zq $(mkTLNatNat 7338241)
+type Zq5 = Zq $(mkTLNatNat 1522160641) -- fit 5 hops: > (last mul)
+type Zq6 = Zq $(mkTLNatNat 1529498881) -- extra for KS: big
+--type Zq7 = Zq $(mkTLNatNat 12579841)
+type ZqList = '[Zq1,Zq2,Zq3,Zq4,Zq5,Zq6] --,Zq7]
+
+type RescaleZqs = '[Zq1,Zq2,Zq3,Zq4,Zq5]
 
 main :: IO ()
 main = do
@@ -79,7 +89,7 @@ main = do
   -- EAC: This needs to have a non-zero output pNoise level!!
   -- EAC: can remove type sig and use ptexpr as the argument to pt2ct below (which infers the signature),
   -- but this requires compiling PT2CT which takes a long time.
-  let (pttunnel :: PT2CT' CTRngs ZqList Gad _, paramsexpr2) = dup $ linear5 @CT @PTRngs @(Z2E K) @(PNoise N9) Proxy
+  let (pttunnel :: PT2CT' CTRngs ZqList Gad _, paramsexpr2) = dup $ linear5 @CT @PTRngs @(Z2E K) @(PNoise N11) Proxy
   putStrLn $ "PT expression params:\n" ++ params pttunnel paramsexpr2
 
   putStrLn $ "PT Composition: " ++ pprint (ex01 .: ex11)
@@ -99,7 +109,7 @@ main = do
                   @ZqList
                   @Gad
                   @Int64)
-                  (linear5 @CT @PTRngs @(Z2E K) @(PNoise N9) Proxy)
+                  (linear5 @CT @PTRngs @(Z2E K) @(PNoise N11) Proxy)
 
     let (r1,r) = dup roundTree
         (r2,r3) = dup r
@@ -125,17 +135,6 @@ main = do
     let (_,errors) = runWriter $ eval (f .: g) (return arg1)
 
     liftIO $ print errors
-
--- these are ~ 2^15
-
-type Zq1 = Zq $(mkTLNatNat 3144961)
-type Zq2 = Zq $(mkTLNatNat 5241601)
-type Zq3 = Zq $(mkTLNatNat 7338241)
-type Zq4 = Zq $(mkTLNatNat 9959041)
-type Zq5 = Zq $(mkTLNatNat 10483201)
-type Zq6 = Zq $(mkTLNatNat 11531521)
-type Zq7 = Zq $(mkTLNatNat 12579841)
-type ZqList = '[Zq1,Zq2,Zq3,Zq4,Zq5,Zq6,Zq7]
 
 
 
