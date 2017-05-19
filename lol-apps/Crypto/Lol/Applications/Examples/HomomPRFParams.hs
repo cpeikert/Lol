83ccca7c4da8a9d4b0357604031ae07e7f1aefde
Eric <ecrockett0@gmail.com>
2017-05-19 19:50:33 -0400

    updated HomomPRFParams

--- lol-apps/Crypto/Lol/Applications/Examples/HomomPRFParams.hs
+++ lol-apps/Crypto/Lol/Applications/Examples/HomomPRFParams.hs
@@ -35,20 +35,17 @@ type H5' = H5
 type RngList = '[ '(H0,H0'), '(H1,H1'), '(H2,H2'), '(H3,H3'), '(H4,H4'), '(H5,H5') ]
 
 type Zq (q :: k) = ZqBasic q Int64
--- three 24-bit moduli, enough to handle rounding for p=32 (depth-4 circuit at ~17 bits per mul)
-type ZQ1 = Zq 18869761
-type ZQ2 = (Zq 19393921, ZQ1)
-type ZQ3 = (Zq 19918081, ZQ2)
-type ZQ4 = (Zq 25159681, ZQ3)
--- a 31-bit modulus, for rounding off after the last four hops
-type ZQ5 = (Zq 2149056001, ZQ4)
--- for rounding off after the first hop
-type ZQ6 = (Zq 3144961, ZQ5)
-type ZQ7 = (Zq 7338241, ZQ6)
-type ZQSeq = '[ZQ7, ZQ6, ZQ5, ZQ4, ZQ3, ZQ2, ZQ1]
 
-type ZP = Zq PP8
-type ZQ = ZQ4 -- if p=2^k, choose ZQ[k+1]
+type ZQ1 = Zq 1520064001
+type ZQ2 = (Zq 3144961, ZQ1)
+type ZQ3 = (Zq 5241601, ZQ2)
+type ZQ4 = (Zq 7338241, ZQ3)
+type ZQ5 = (Zq 1522160641, ZQ4)
+type ZQ6 = (Zq 1529498881, ZQ5)
+type ZQSeq = '[ZQ6, ZQ5, ZQ4, ZQ3, ZQ2, ZQ1]
+
+type ZP = Zq PP16
+type ZQ = ZQ5 -- if p=2^k, choose ZQ[k+1]
 
 -- these need not be the same
 type KSGad = BaseBGad 2
