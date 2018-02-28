04fe7de8e75f5c5447409c621b16bc6505014348
Chris Peikert <cpeikert@alum.mit.edu>
2018-02-28 17:08:29 -0500

    updated to new Cyclotomic interfaces

--- lol/Crypto/Lol/Cyclotomic/Linear.hs
+++ lol/Crypto/Lol/Cyclotomic/Linear.hs
@@ -2,10 +2,10 @@
 Module      : Crypto.Lol.Cyclotomic.Linear
 Description : Functions from one cyclotomic ring to another that are linear
               over a common subring.
-Copyright   : (c) Eric Crockett, 2011-2017
-                  Chris Peikert, 2011-2017
+Copyright   : (c) Eric Crockett, 2011-2018
+                  Chris Peikert, 2011-2018
 License     : GPL-3
-Maintainer  : ecrockett0@email.com
+Maintainer  : ecrockett0@gmail.com
 Stability   : experimental
 Portability : POSIX
 
@@ -18,6 +18,7 @@ over a common subring.
 {-# LANGUAGE ConstraintKinds            #-}
 {-# LANGUAGE DataKinds                  #-}
 {-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE KindSignatures             #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
@@ -34,12 +35,12 @@ module Crypto.Lol.Cyclotomic.Linear
 , linearDec, evalLin, extendLin
 ) where
 
-import Crypto.Lol.Cyclotomic.Cyc
+import Crypto.Lol.Cyclotomic.Language
 import Crypto.Lol.Prelude
 import Crypto.Lol.Reflects
 import Crypto.Lol.Types.Proto
-import Crypto.Proto.Lol.RqProduct (RqProduct)
-import Crypto.Proto.Lol.LinearRq (LinearRq(LinearRq))
+import Crypto.Proto.Lol.LinearRq      (LinearRq (LinearRq))
+import Crypto.Proto.Lol.RqProduct     (RqProduct)
 
 import Algebra.Additive as Additive (C)
 
@@ -52,19 +53,20 @@ import Data.Word
 
 -- CJP: also have constructor for relative Pow basis of R/E?  So far
 -- not needed.
-newtype Linear t z (e::Factored) (r::Factored) (s::Factored) = RD [Cyc t s z]
+newtype Linear c (e::Factored) (r::Factored) (s::Factored) z = RD [c s z]
+  deriving Show
 
-deriving instance (NFData (Cyc t s z)) => NFData (Linear t z e r s)
+deriving instance NFData (c s z) => NFData (Linear c e r s z)
 
 -- some params are phantom but matter for safety
-type role Linear representational nominal representational representational nominal
+type role Linear representational representational representational nominal nominal
 
 -- | Construct an \(E\)-linear function given a list of its output values
 -- (in \(S\)) on the relative decoding basis of \(R/E\).  The number of
 -- elements in the list must not exceed the size of the basis.
-linearDec :: forall t z e r s .
-             (e `Divides` r, e `Divides` s, CElt t z)
-             => [Cyc t s z] -> Linear t z e r s
+linearDec :: forall c e r s z .
+             (e `Divides` r, e `Divides` s, ExtensionCyc c z)
+             => [c s z] -> Linear c e r s z
 linearDec ys = let ps = proxy powBasis (Proxy::Proxy e) `asTypeOf` ys
                in if length ys <= length ps then RD (adviseCRT <$> ys)
                else error $ "linearDec: too many entries: "
@@ -72,13 +74,12 @@ linearDec ys = let ps = proxy powBasis (Proxy::Proxy e) `asTypeOf` ys
                         ++ show (length ps)
 
 -- | Evaluates the given linear function on the input.
-evalLin :: forall t z e r s .
-           (e `Divides` r, e `Divides` s, CElt t z)
-           => Linear t z e r s -> Cyc t r z -> Cyc t s z
-evalLin (RD ys) r = sum (zipWith (*) ys $
-                         embed <$> (coeffsDec r :: [Cyc t e z]))
+evalLin :: forall c e r s z .
+           (e `Divides` r, e `Divides` s, Ring (c s z), ExtensionCyc c z)
+           => Linear c e r s z -> c r z -> c s z
+evalLin (RD ys) r = sum (zipWith (*) ys $ embed <$> (coeffsDec r :: [c e z]))
 
-instance Additive (Cyc t s z) => Additive.C (Linear t z e r s) where
+instance Additive (c s z) => Additive.C (Linear c e r s z) where
   zero = RD []
 
   (RD as) + (RD bs) = RD $ sumall as bs
@@ -88,16 +89,15 @@ instance Additive (Cyc t s z) => Additive.C (Linear t z e r s) where
 
   negate (RD as) = RD $ negate <$> as
 
-instance (Reduce z zq, Fact s, CElt t z, CElt t zq)
-         => Reduce (Linear t z e r s) (Linear t zq e r s) where
+instance (Reduce (c s z) (c s zp))
+         => Reduce (Linear c e r s z) (Linear c e r s zp) where
   reduce (RD ys) = RD $ reduce <$> ys
 
-type instance LiftOf (Linear t zp e r s) = Linear t (LiftOf zp) e r s
+type instance LiftOf (Linear c e r s zp) = Linear c e r s (LiftOf zp)
 
--- | lifts with respect to powerful basis, for best geometry
-instance (CElt t zp, CElt t z, z ~ LiftOf zp, Lift zp z, Fact s)
-         => Lift' (Linear t zp e r s) where
-  lift (RD ys) = RD $ liftPow <$> ys
+-- | lifting in powerful basis is generally best, geometrically
+instance (LiftCyc c zp) => LiftCyc (Linear c e r) zp where
+  liftCyc b (RD ys) = RD $ liftCyc b <$> ys
 
 -- | A convenient constraint synonym for extending a linear function
 -- to larger rings.
@@ -110,8 +110,8 @@ type ExtendLinIdx e r s e' r' s' =
 -- \(\lcm(r,e') | r'\) (not equality), but this generality would
 -- significantly complicate the implementation, and for our purposes
 -- there's no reason to use any larger \(r'\).)
-extendLin :: (ExtendLinIdx e r s e' r' s')
-           => Linear t z e r s -> Linear t z e' r' s'
+extendLin :: (ExtendLinIdx e r s e' r' s', ExtensionCyc c z)
+           => Linear c e r s z -> Linear c e' r' s' z
 -- CJP: this simple implementation works because R/E and R'/E' have
 -- identical decoding bases, because R' \cong R \otimes_E E'.  If we
 -- relax the constraint on E then we'd have to change the
@@ -119,10 +119,14 @@ extendLin :: (ExtendLinIdx e r s e' r' s')
 extendLin (RD ys) = RD (embed <$> ys)
 
 instance (Reflects e Word32, Reflects r Word32,
-          Protoable (Cyc t s zq), ProtoType (t s zq) ~ RqProduct)
-  => Protoable (Linear t zq e r s) where
-  type ProtoType (Linear t zq e r s) = LinearRq
-  toProto (RD cs) = LinearRq (proxy value (Proxy::Proxy e)) (proxy value (Proxy::Proxy r)) $ toProto cs
+          Protoable (c s zq), ProtoType (c s zq) ~ RqProduct)
+  => Protoable (Linear c e r s zq) where
+
+  type ProtoType (Linear c e r s zq) = LinearRq
+
+  toProto (RD cs) =
+    LinearRq (proxy value (Proxy::Proxy e)) (proxy value (Proxy::Proxy r)) $ toProto cs
+
   fromProto (LinearRq e r cs) =
     let e' = proxy value (Proxy::Proxy e)
         r' = proxy value (Proxy::Proxy r)
