090c763ace1449b358fcfe6dc94a0d220ff6a878
Michael Rosenberg <micro@fastmail.com>
2018-05-31 19:22:09 -0400

    Updated CycTests to use new Cyc/CycRep API; does not compile yet; we need to expose crtSet from Cyc.hs

--- lol/Crypto/Lol/Tests/CycTests.hs
+++ lol/Crypto/Lol/Tests/CycTests.hs
@@ -33,53 +33,51 @@ import Crypto.Lol.Utils.ShowType
 import Crypto.Lol.Utils.Tests
 
 import qualified Test.Framework as TF
+import qualified Test.QuickCheck as QC
 
 -- | Tests for single-index 'Cyc' operations. There must be a CRT basis for \(O_m\) over @r@.
-cycTests1 :: forall t m r . _ => Proxy '(m,r) -> Proxy t -> TF.Test
-cycTests1 _ _ =
-  let ptmr = Proxy :: Proxy '(t,m,r)
-  in testGroup (showType ptmr) $ ($ ptmr) <$> [
-      genTestArgs "mulGPow" prop_mulgPow,
-      genTestArgs "mulGDec" prop_mulgDec,
-      genTestArgs "mulGCRT" prop_mulgCRT
-      ]
+cycTests1 :: forall t m r . (Fact m, _) => QC.Gen (Cyc t m r) -> TF.Test
+cycTests1 cycGen =
+  let ptmr = Proxy :: Proxy '(t,m,r) in
+  TF.testGroup (showType ptmr) $ ($ cycGen) <$> [
+   testWithGen "mulGPow" prop_mulgPow,
+   testWithGen "mulGDec" prop_mulgDec,
+   testWithGen "mulGCRT" prop_mulgCRT]
 
 -- | Tests for inter-ring 'Cyc' operations. There must be a CRT basis for \(O_{m'}\) over @r@.
-cycTests2 :: forall t m m' r . _ => Proxy '(m,m',r) -> Proxy t -> TF.Test
-cycTests2 _ _ =
-  let ptmr = Proxy :: Proxy '(t,m,m',r)
-  in testGroup (showType ptmr) $ ($ ptmr) <$> [
-      genTestArgs "crtSet" prop_crtSet_pairs,
-      genTestArgs "coeffsPow" prop_coeffsBasis
-      ]
+cycTests2 :: forall t m m' r . _ => Proxy '(t,m,m',r) -> QC.Gen (Cyc t m' r) -> TF.Test
+cycTests2 _ cycGen =
+  let ptmmr = Proxy::Proxy '(t,m,m',r)
+  in TF.testGroup (showType ptmmr) [
+      testWithoutGen "crtSet" (prop_crtSet_pairs ptmmr),
+      testWithGen "coeffsPow" (prop_coeffsBasis ptmmr) cycGen]
 
-prop_mulgPow :: _ => Cyc t m r -> Test '(t,m,r)
+prop_mulgPow :: _ => Cyc t m r -> Bool
 prop_mulgPow x =
   let y = advisePow x
-  in test $ y == (fromJust' "prop_mulgPow failed divisibility!" $ divG $ mulG y)
+  in y == (fromJust' "prop_mulgPow failed divisibility!" $ divG $ mulG y)
 
-prop_mulgDec :: _ => Cyc t m r -> Test '(t,m,r)
+prop_mulgDec :: _ => Cyc t m r -> Bool
 prop_mulgDec x =
-  let y = adviseDec x
-  in test $ y == (fromJust' "prop_mulgDec failed divisibility!" $ divG $ mulG y)
+  let y = adviseDec x in
+  y == (fromJust' "prop_mulgDec failed divisibility!" $ divG $ mulG y)
 
-prop_mulgCRT :: _ => Cyc t m r -> Test '(t,m,r)
+prop_mulgCRT :: _ => Cyc t m r -> Bool
 prop_mulgCRT x =
-  let y = adviseCRT x
-  in test $ y == (fromJust' "prop_mulgCRT failed divisibility!" $ divG $ mulG y)
+  let y = adviseCRT x in
+  y == (fromJust' "prop_mulgCRT failed divisibility!" $ divG $ mulG y)
 
-prop_coeffsBasis :: forall t m m' r . _
-  => Cyc t m' r -> Test '(t,m,m',r)
-prop_coeffsBasis x =
+prop_coeffsBasis :: forall t m m' r . _ => Proxy '(t,m,m',r) -> Cyc t m' r -> Bool
+prop_coeffsBasis _ x =
   let xs = map embed (coeffsCyc Pow x :: [Cyc t m r])
       bs = proxy powBasis (Proxy::Proxy m)
-  in test $ (sum $ zipWith (*) xs bs) == x
+  in (sum $ zipWith (*) xs bs) == x
 
 -- verifies that CRT set elements satisfy c_i * c_j = delta_ij * c_i
 -- necessary (but not sufficient) condition
-prop_crtSet_pairs :: forall t m m' r . (CElt t r, Fact m', _)
-  => Test '(t,m,m',r)
-prop_crtSet_pairs =
+prop_crtSet_pairs :: forall (t :: Factored -> * -> *) m (m' :: Factored) (r :: *) . _
+                  => Proxy '(t,m,m',r) -> Bool
+prop_crtSet_pairs _ =
   let crtset = proxy crtSet (Proxy::Proxy m) :: [Cyc t m' r]
       pairs = join (liftM2 (,)) crtset
-  in test $ and $ map (\(a,b) -> if a == b then a*b == a else a*b == zero) pairs
+  in and $ map (\(a,b) -> if a == b then a*b == a else a*b == zero) pairs
