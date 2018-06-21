dc2566f086f1003e9c80e5ab986bfa3e189717de
bmanga <bmanga@umich.edu>
2018-06-21 15:38:06 -0400

    Removed unnecessary pragmas and imports

--- lol-apps/Crypto/Lol/Applications/Examples/KHPRF.hs
+++ lol-apps/Crypto/Lol/Applications/Examples/KHPRF.hs
@@ -1,10 +1,10 @@
 {-|
 Module      : Crypto.Lol.Applications.Examples.KHPRF
 Description : Example using KeyHomomorphicPRF.
-Copyright   : (c) Eric Crockett, 2011-2017
-                  Chris Peikert, 2011-2017
+Copyright   : (c) Chris Peikert, 2018
+                  Bogdan Manga,  2018
 License     : GPL-3
-Maintainer  : ecrockett0@email.com
+Maintainer  : bmanga@umich.edu
 Stability   : experimental
 Portability : POSIX
 
@@ -12,30 +12,13 @@ Example using KeyHomomorphicPRF.
 -}
 
 {-# LANGUAGE DataKinds            #-}
-{-# LANGUAGE FlexibleContexts     #-}
-{-# LANGUAGE FlexibleInstances    #-}
-{-# LANGUAGE KindSignatures       #-}
-{-# LANGUAGE GADTs                #-}
-{-# LANGUAGE NoImplicitPrelude    #-}
-{-# LANGUAGE PolyKinds            #-}
 {-# LANGUAGE ScopedTypeVariables  #-}
-{-# LANGUAGE StandaloneDeriving   #-}
-{-# LANGUAGE TemplateHaskell      #-}
-{-# LANGUAGE TypeFamilies         #-}
-{-# LANGUAGE TypeOperators        #-}
-{-# LANGUAGE UndecidableInstances #-}
 
-module Crypto.Lol.Applications.Examples.KHPRF
-(khprfMain
-) where
+module Crypto.Lol.Applications.Examples.KHPRF (khprfMain) where
 
 import Crypto.Lol hiding (replicate)
 import Crypto.Lol.Applications.KeyHomomorphicPRF
 import Crypto.Lol.Types
-
-import Control.Applicative
-import Control.Monad.Reader
-import Control.Monad.State
 
 type Top = 'Intern ('Intern 'Leaf 'Leaf) 'Leaf
 type N = 16
