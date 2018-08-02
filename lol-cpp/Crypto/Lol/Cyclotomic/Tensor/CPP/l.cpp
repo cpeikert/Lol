4e02da94bc02419fa8330a1dbbf0d9e5b6fa0688
Chris Peikert <cpeikert@alum.mit.edu>
2018-08-02 16:57:30 -0400

    renamed U->L

--- lol-cpp/Crypto/Lol/Cyclotomic/Tensor/CPP/l.cpp
+++ lol-cpp/Crypto/Lol/Cyclotomic/Tensor/CPP/l.cpp
@@ -1,5 +1,5 @@
 /*
-Module      : u.cpp
+Module      : l.cpp
 Description : Powerful <-> Decoding basis conversion.
 Copyright   : (c) Eric Crockett, 2011-2017
                   Chris Peikert, 2011-2017
@@ -8,10 +8,10 @@ Maintainer  : ecrockett0@email.com
 Stability   : experimental
 Portability : POSIX
 
-Prime- and arbitrary-index transformations for the U operator
+Prime- and arbitrary-index transformations for the L operator
 (see LPR13: https://eprint.iacr.org/2013/293) which converts between the
 powerful and decoding basis representation for cyclotomic ring elements.
-U converts decoding -> powerful represenation, U^{-1} is the reverse.
+L converts decoding -> powerful represenation, L^{-1} is the reverse.
 */
 
 #include "types.h"
@@ -21,15 +21,15 @@ U converts decoding -> powerful represenation, U^{-1} is the reverse.
  * abelian group) to powerful basis coefficients.
  * 'y' is an array of decoding basis coefficients in a three-dimensional tensor:
  * [lts]x[rts]x[p-1].
- * We can think of the operator as  (I_lts \otimes U_p \otimes I_rts).
+ * We can think of the operator as  (I_lts \otimes L_p \otimes I_rts).
  */
-template <typename abgrp> void up (abgrp* y, hDim_t lts, hDim_t rts, hDim_t p)
+template <typename abgrp> void lp (abgrp* y, hDim_t lts, hDim_t rts, hDim_t p)
 {
-  // U_2 = id
+  // L_2 = id
   if(p == 2) {return;}
 
   // operate on the chunk of 'y' corresponding to each matrix on the diagonal.
-  // each square diagonal matrix in I_lts \otimes U_p \otimes I_rts has
+  // each square diagonal matrix in I_lts \otimes L_p \otimes I_rts has
   // size (p-1)*rts
   // lidx is the offset into y corresponding to the block diagonal matrix
   for (hDim_t lblock = 0, lidx = 0; lblock < lts; ++lblock, lidx += (p-1)*rts) {
@@ -50,15 +50,15 @@ template <typename abgrp> void up (abgrp* y, hDim_t lts, hDim_t rts, hDim_t p)
  * abelian group) to powerful basis coefficients.
  * 'y' is an array of powerful basis coefficients in a three-dimensional tensor:
  * [lts]x[rts]x[p-1].
- * We can think of the operator as  I_lts \otimes U_p^{-1} \otimes I_rts).
+ * We can think of the operator as  I_lts \otimes L_p^{-1} \otimes I_rts).
  */
-template <typename abgrp> void upInv (abgrp* y, hDim_t lts, hDim_t rts, hDim_t p)
+template <typename abgrp> void lpInv (abgrp* y, hDim_t lts, hDim_t rts, hDim_t p)
 {
-  // (U_2)^{-1} = id
+  // (L_2)^{-1} = id
   if(p == 2) {return;}
 
   // operate on the chunk of 'y' corresponding to each matrix on the diagonal.
-  // each square diagonal matrix in I_lts \otimes (U_p \otimes I_rts) has
+  // each square diagonal matrix in I_lts \otimes (L_p \otimes I_rts) has
   // size (p-1)*rts
   // lidx is the offset into y corresponding to the block diagonal matrix
   for (hDim_t lblock = 0, lidx = 0; lblock < lts; ++lblock, lidx += (p-1)*rts) {
@@ -83,10 +83,10 @@ template <typename abgrp> void upInv (abgrp* y, hDim_t lts, hDim_t rts, hDim_t p
  * Use "extern "C"" to avoid C++ name mangling, which makes it hard to call
  * from Haskell.
  */
-extern "C" void tensorURq (Zq* y, hDim_t totm, PrimeExponent* peArr, hShort_t sizeOfPE, hInt_t q)
+extern "C" void tensorLRq (Zq* y, hDim_t totm, PrimeExponent* peArr, hShort_t sizeOfPE, hInt_t q)
 {
   Zq::q = q;
-  tensorFuserPrime (y, up, totm, peArr, sizeOfPE, q);
+  tensorFuserPrime (y, lp, totm, peArr, sizeOfPE, q);
   // Haskell expects each Z_q coefficient to be in the range 0 <= x < q_i, so
   // ensure that is the case.
   canonicalizeZq(y,totm,q);
@@ -100,25 +100,25 @@ extern "C" void tensorURq (Zq* y, hDim_t totm, PrimeExponent* peArr, hShort_t si
  * Use "extern "C"" to avoid C++ name mangling, which makes it hard to call
  * from Haskell.
  */
-extern "C" void tensorUR (hInt_t* y, hDim_t totm, PrimeExponent* peArr, hShort_t sizeOfPE)
+extern "C" void tensorLR (hInt_t* y, hDim_t totm, PrimeExponent* peArr, hShort_t sizeOfPE)
 {
-  tensorFuserPrime (y, up, totm, peArr, sizeOfPE, 0);
+  tensorFuserPrime (y, lp, totm, peArr, sizeOfPE, 0);
 }
 
-extern "C" void tensorUDouble (double* y, hDim_t totm, PrimeExponent* peArr, hShort_t sizeOfPE)
+extern "C" void tensorLDouble (double* y, hDim_t totm, PrimeExponent* peArr, hShort_t sizeOfPE)
 {
-  tensorFuserPrime (y, up, totm, peArr, sizeOfPE, 0);
+  tensorFuserPrime (y, lp, totm, peArr, sizeOfPE, 0);
 }
 
-extern "C" void tensorURRq (RRq* y, hDim_t totm, PrimeExponent* peArr, hShort_t sizeOfPE)
+extern "C" void tensorLRRq (RRq* y, hDim_t totm, PrimeExponent* peArr, hShort_t sizeOfPE)
 {
-  tensorFuserPrime (y, up, totm, peArr, sizeOfPE, 0);
+  tensorFuserPrime (y, lp, totm, peArr, sizeOfPE, 0);
   canonicalizeRRq(y,totm);
 }
 
-extern "C" void tensorUC (Complex* y, hDim_t totm, PrimeExponent* peArr, hShort_t sizeOfPE)
+extern "C" void tensorLC (Complex* y, hDim_t totm, PrimeExponent* peArr, hShort_t sizeOfPE)
 {
-  tensorFuserPrime (y, up, totm, peArr, sizeOfPE, 0);
+  tensorFuserPrime (y, lp, totm, peArr, sizeOfPE, 0);
 }
 
 /* Arbitrary-index transformation that converts powerful basis coefficients
@@ -129,10 +129,10 @@ extern "C" void tensorUC (Complex* y, hDim_t totm, PrimeExponent* peArr, hShort_
  * Use "extern "C"" to avoid C++ name mangling, which makes it hard to call
  * from Haskell.
  */
-extern "C" void tensorUInvRq (Zq* y, hDim_t totm, PrimeExponent* peArr, hShort_t sizeOfPE, hInt_t q)
+extern "C" void tensorLInvRq (Zq* y, hDim_t totm, PrimeExponent* peArr, hShort_t sizeOfPE, hInt_t q)
 {
   Zq::q = q;
-  tensorFuserPrime (y, upInv, totm, peArr, sizeOfPE, q);
+  tensorFuserPrime (y, lpInv, totm, peArr, sizeOfPE, q);
   // Haskell expects each Z_q coefficient to be in the range 0 <= x < q_i, so
   // ensure that is the case.
   canonicalizeZq(y,totm,q);
@@ -146,23 +146,23 @@ extern "C" void tensorUInvRq (Zq* y, hDim_t totm, PrimeExponent* peArr, hShort_t
  * Use "extern "C"" to avoid C++ name mangling, which makes it hard to call
  * from Haskell.
  */
-extern "C" void tensorUInvR (hInt_t* y, hDim_t totm, PrimeExponent* peArr, hShort_t sizeOfPE)
+extern "C" void tensorLInvR (hInt_t* y, hDim_t totm, PrimeExponent* peArr, hShort_t sizeOfPE)
 {
-  tensorFuserPrime (y, upInv, totm, peArr, sizeOfPE, 0);
+  tensorFuserPrime (y, lpInv, totm, peArr, sizeOfPE, 0);
 }
 
-extern "C" void tensorUInvDouble (double* y, hDim_t totm, PrimeExponent* peArr, hShort_t sizeOfPE)
+extern "C" void tensorLInvDouble (double* y, hDim_t totm, PrimeExponent* peArr, hShort_t sizeOfPE)
 {
-  tensorFuserPrime (y, upInv, totm, peArr, sizeOfPE, 0);
+  tensorFuserPrime (y, lpInv, totm, peArr, sizeOfPE, 0);
 }
 
-extern "C" void tensorUInvRRq (RRq* y, hDim_t totm, PrimeExponent* peArr, hShort_t sizeOfPE)
+extern "C" void tensorLInvRRq (RRq* y, hDim_t totm, PrimeExponent* peArr, hShort_t sizeOfPE)
 {
-  tensorFuserPrime (y, upInv, totm, peArr, sizeOfPE, 0);
+  tensorFuserPrime (y, lpInv, totm, peArr, sizeOfPE, 0);
   canonicalizeRRq(y,totm);
 }
 
-extern "C" void tensorUInvC (Complex* y, hDim_t totm, PrimeExponent* peArr, hShort_t sizeOfPE)
+extern "C" void tensorLInvC (Complex* y, hDim_t totm, PrimeExponent* peArr, hShort_t sizeOfPE)
 {
-  tensorFuserPrime (y, upInv, totm, peArr, sizeOfPE, 0);
+  tensorFuserPrime (y, lpInv, totm, peArr, sizeOfPE, 0);
 }
