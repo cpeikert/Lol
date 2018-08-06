ec7810d4abcc36f20196747b36fed23b184fcda6
Chris Peikert <cpeikert@alum.mit.edu>
2018-08-06 18:15:59 -0400

    removed tags from types for type applications

--- lol/Crypto/Lol/Reflects.hs
+++ lol/Crypto/Lol/Reflects.hs
@@ -11,6 +11,7 @@ Portability : POSIX
 Generic interface for reflecting types to values.
 -}
 
+{-# LANGUAGE AllowAmbiguousTypes   #-}
 {-# LANGUAGE DataKinds             #-}
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
@@ -19,24 +20,23 @@ Generic interface for reflecting types to values.
 {-# LANGUAGE NoImplicitPrelude     #-}
 {-# LANGUAGE PolyKinds             #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeApplications      #-}
 {-# LANGUAGE UndecidableInstances  #-}
 
 module Crypto.Lol.Reflects
-( Reflects(..),
+( Reflects(..)
 ) where
 
+import Algebra.Ring      as Ring
 import Algebra.ToInteger as ToInteger
-import Algebra.Ring as Ring
 import NumericPrelude
 
 import Crypto.Lol.Factored
 
-import Control.Applicative
-import Data.Functor.Trans.Tagged
 import Data.Proxy
 import Data.Reflection
 import Data.Singletons
-import GHC.TypeLits              as TL
+import GHC.TypeLits    as TL
 
 -- | Reflection without fundep, and with tagged value. Intended only
 -- for low-level code; build specialized wrappers around it for
@@ -44,26 +44,26 @@ import GHC.TypeLits              as TL
 
 class Reflects a i where
   -- | Reflect the value assiated with the type @a@.
-  value :: Tagged a i
+  value :: i
 
 instance (KnownNat a, Ring.C i) => Reflects (a :: TL.Nat) i where
-  value = tag $ fromIntegral $ natVal (Proxy::Proxy a)
+  value = fromIntegral $ natVal (Proxy::Proxy a)
 
 instance (PosC a, ToInteger.C i) => Reflects a i where
-  value = tag $ posToInt $ fromSing (sing :: Sing a)
+  value = posToInt $ fromSing (sing :: Sing a)
 
 instance (BinC a, ToInteger.C i) => Reflects a i where
-  value = tag $ binToInt $ fromSing (sing :: Sing a)
+  value = binToInt $ fromSing (sing :: Sing a)
 
 instance (Prime p, ToInteger.C i) => Reflects p i where
-  value = fromIntegral <$> valuePrime
+  value = fromIntegral $ valuePrime @p
 
 instance (PPow pp, ToInteger.C i) => Reflects pp i where
-  value = fromIntegral <$> valuePPow
+  value = fromIntegral $ valuePPow @pp
 
 instance (Fact m, ToInteger.C i) => Reflects m i where
-  value = fromIntegral <$> valueFact
+  value = fromIntegral $ valueFact @m
 
 instance (Reifies q i, ToInteger.C i, Ring.C r)
   => Reflects (q :: *) r where
-  value = tag $ fromIntegral $ reflect (Proxy::Proxy q)
+  value = fromIntegral $ reflect (Proxy::Proxy q)
