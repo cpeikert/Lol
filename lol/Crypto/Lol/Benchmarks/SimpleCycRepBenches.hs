99bbfb299e82a07db00590b890e5564eac36e243
Chris Peikert <cpeikert@alum.mit.edu>
2018-08-10 11:34:01 -0400

    updated UCyc -> CycRep

--- lol/Crypto/Lol/Benchmarks/SimpleCycRepBenches.hs
+++ lol/Crypto/Lol/Benchmarks/SimpleCycRepBenches.hs
@@ -1,6 +1,6 @@
 {-|
-Module      : Crypto.Lol.Benchmarks.SimpleUCycBenches
-Description : Benchmarks for the 'UCyc' interface, without benchmark harness.
+Module      : Crypto.Lol.Benchmarks.SimpleCycRepBenches
+Description : Benchmarks for the 'CycRep' interface, without benchmark harness.
 Copyright   : (c) Eric Crockett, 2011-2017
                   Chris Peikert, 2011-2017
 License     : GPL-3
@@ -8,7 +8,7 @@ Maintainer  : ecrockett0@email.com
 Stability   : experimental
 Portability : POSIX
 
-Benchmarks for the 'UCyc' interface, without benchmark
+Benchmarks for the 'CycRep' interface, without benchmark
 harness (for performance comparison).
 -}
 
@@ -18,73 +18,76 @@ harness (for performance comparison).
 {-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
 {-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
 
 {-# OPTIONS_GHC -fno-warn-partial-type-signatures #-}
 
-module Crypto.Lol.Benchmarks.SimpleUCycBenches (simpleUCycBenches1, simpleUCycBenches2) where
+module Crypto.Lol.Benchmarks.SimpleCycRepBenches
+( simpleCycRepBenches1, simpleCycRepBenches2
+) where
 
 import Control.Applicative
 import Control.Monad.Random hiding (lift)
 
+import Crypto.Lol.Cyclotomic.CycRep
 import Crypto.Lol.Prelude
-import Crypto.Lol.Cyclotomic.UCyc
 import Crypto.Lol.Types
 import Crypto.Random
 
 import Criterion
 
 -- | Benchmarks for single-index operations. There must be a CRT basis for \(O_m\) over @r@.
-{-# INLINE simpleUCycBenches1 #-}
-simpleUCycBenches1 :: forall t m r (gen :: *) . _
+{-# INLINE simpleCycRepBenches1 #-}
+simpleCycRepBenches1 :: forall t m r (gen :: *) . _
   => Proxy '(t,m,r) -> Proxy gen -> IO Benchmark
-simpleUCycBenches1 _ _ = do
-  x1 :: UCyc t m P (r, r) <- getRandom
+simpleCycRepBenches1 _ _ = do
+  x1 :: CycRep t P m (r, r) <- getRandom
   let x1' = toDec x1
-  (Right x2) :: UCycPC t m (r, r) <- getRandom
-  x3 :: UCycEC t m r <- pcToEC <$> getRandom
-  x4 :: UCyc t m P r <- getRandom
+  (Right x2) :: CycRepPC t m (r, r) <- getRandom
+  x3 :: CycRepEC t m r <- pcToEC <$> getRandom
+  x4 :: CycRep t P m r <- getRandom
   let x5 = toDec x4
-  (Right x6) :: UCycPC t m r <- getRandom
-  let x4' = mulG x4
-      x5' = mulG x5
+  (Right x6) :: CycRepPC t m r <- getRandom
+  let x4' = mulGPow x4
+      x5' = mulGDec x5
   gen <- newGenIO
-  return $ bgroup "SUCyc" [
-    bench "unzipPow"    $ nf unzipPow x1,
-    bench "unzipDec"    $ nf unzipDec x1',
-    bench "unzipCRT"    $ nf unzipCRTC x2,
+  return $ bgroup "SCycRep" [
+--    bench "unzipPow"    $ nf unzipPow x1,
+--    bench "unzipDec"    $ nf unzipDec x1',
+--    bench "unzipCRT"    $ nf unzipCRTC x2,
     bench "zipWith (*)" $ nf (x3*) x3,
     bench "crt"         $ nf toCRT x4,
     bench "crtInv"      $ nf toPow x6,
     bench "l"           $ nf toPow x5,
     bench "lInv"        $ nf toDec x4,
-    bench "*g Pow"      $ nf mulG x4,
-    bench "*g Dec"      $ nf mulG x5,
-    bench "*g CRT"      $ nf mulG x6,
+    bench "*g Pow"      $ nf mulGPow x4,
+    bench "*g Dec"      $ nf mulGDec x5,
+    bench "*g CRT"      $ nf mulGCRTC x6,
     bench "divg Pow"    $ nf divGPow x4',
     bench "divg Dec"    $ nf divGDec x5',
     bench "divg CRT"    $ nf divGCRTC x6,
     bench "lift"        $ nf lift x4,
-    bench "error"       $ nf (evalRand (errorRounded (0.1 :: Double) :: Rand (CryptoRand gen) (UCyc t m D Int64))) gen
+    bench "error"       $ nf (evalRand (roundedGaussian (0.1 :: Double) :: Rand (CryptoRand gen) (CycRep t D m Int64))) gen
     ]
 
 -- | Benchmarks for inter-ring operations. There must be a CRT basis for \(O_{m'}\) over @r@.
-{-# INLINE simpleUCycBenches2 #-}
-simpleUCycBenches2 :: forall t m m' r . _ => Proxy '(t,m,m',r) -> IO Benchmark
-simpleUCycBenches2 _ = do
-  x4 :: UCyc t m' P r <- getRandom
+{-# INLINE simpleCycRepBenches2 #-}
+simpleCycRepBenches2 :: forall t m m' r . (m `Divides` m', _) => Proxy '(t,m,m',r) -> IO Benchmark
+simpleCycRepBenches2 _ = do
+  x4 :: CycRep t P m' r <- getRandom
   let x5 = toDec x4
-  (Right x6) :: UCycPC t m' r <- getRandom
-  x7 :: UCyc t m P r <- getRandom
+  (Right x6) :: CycRepPC t m' r <- getRandom
+  x7 :: CycRep t P m r <- getRandom
   let x8 = toDec x7
-  (Right x9) :: UCycPC t m r <- getRandom
-  return $ bgroup "SUCyc" [
-    bench "twacePow" $ nf (twacePow :: UCyc t m' P r -> UCyc t m P r) x4,
-    bench "twaceDec" $ nf (twaceDec :: UCyc t m' D r -> UCyc t m D r) x5,
-    bench "twaceCRT" $ nf (twaceCRTC :: UCyc t m' C r -> UCycPC t m r) x6,
-    bench "embedPow" $ nf (embedPow :: UCyc t m P r -> UCyc t m' P r) x7,
-    bench "embedDec" $ nf (embedDec :: UCyc t m D r -> UCyc t m' D r) x8,
-    bench "embedCRT" $ nf (embedCRTC :: UCyc t m C r -> UCycPC t m' r) x9
+  (Right x9) :: CycRepPC t m r <- getRandom
+  return $ bgroup "SCycRep" [
+    bench "twacePow" $ nf (twacePow  :: CycRep t P m' r -> CycRep t P m  r) x4,
+    bench "twaceDec" $ nf (twaceDec  :: CycRep t D m' r -> CycRep t D m  r) x5,
+    bench "twaceCRT" $ nf (twaceCRTC :: CycRep t C m' r -> CycRepPC t m  r) x6,
+    bench "embedPow" $ nf (embedPow  :: CycRep t P m  r -> CycRep t P m' r) x7,
+--    bench "embedDec" $ nf (embedDec  :: CycRep t D m  r -> CycRep t D m' r) x8,
+    bench "embedCRT" $ nf (embedCRTC :: CycRep t C m  r -> CycRepPC t m' r) x9
     ]
 
-pcToEC :: UCycPC t m r -> UCycEC t m r
+pcToEC :: CycRepPC t m r -> CycRepEC t m r
 pcToEC (Right x) = (Right x)
