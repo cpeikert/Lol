5f466c30f2dd5935b5df1a77cced86a9de222b03
Chris Peikert <cpeikert@alum.mit.edu>
2018-08-10 12:28:21 -0400

    hlint

--- lol/Crypto/Lol/Tests/CycTests.hs
+++ lol/Crypto/Lol/Tests/CycTests.hs
@@ -17,20 +17,21 @@ Tests for the 'Cyc' interface.
 {-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE PolyKinds             #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
-{-# LANGUAGE TypeOperators         #-}
 {-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
 
 {-# OPTIONS_GHC -fno-warn-partial-type-signatures #-}
 
 module Crypto.Lol.Tests.CycTests (cycTests1, cycTests2) where
 
 import Control.Applicative
-import Control.Monad (liftM2,join)
+import Control.Monad       (join, liftM2)
 
 import Crypto.Lol
 
 import Crypto.Lol.Utils.ShowType
-import Crypto.Lol.Utils.Tests (testGroup, testWithGen, testWithoutGen, Gen, Test)
+import Crypto.Lol.Utils.Tests    (Gen, Test, testGroup, testWithGen,
+                                  testWithoutGen)
 
 -- | Tests for single-index 'Cyc' operations. There must be a CRT basis for \(O_m\) over @r@.
 cycTests1 :: forall t m r . (Fact m, _) => Gen (Cyc t m r) -> Test
@@ -52,23 +53,23 @@ cycTests2 _ cycGen =
 prop_mulgPow :: _ => Cyc t m r -> Bool
 prop_mulgPow x =
   let y = advisePow x
-  in y == (fromJust' "prop_mulgPow failed divisibility!" $ divG $ mulG y)
+  in y == fromJust' "prop_mulgPow failed divisibility!" (divG $ mulG y)
 
 prop_mulgDec :: _ => Cyc t m r -> Bool
 prop_mulgDec x =
   let y = adviseDec x in
-  y == (fromJust' "prop_mulgDec failed divisibility!" $ divG $ mulG y)
+  y == fromJust' "prop_mulgDec failed divisibility!" (divG $ mulG y)
 
 prop_mulgCRT :: _ => Cyc t m r -> Bool
 prop_mulgCRT x =
   let y = adviseCRT x in
-  y == (fromJust' "prop_mulgCRT failed divisibility!" $ divG $ mulG y)
+  y == fromJust' "prop_mulgCRT failed divisibility!" (divG $ mulG y)
 
 prop_coeffsBasis :: forall t m m' r . _ => Proxy '(t,m,m',r) -> Cyc t m' r -> Bool
 prop_coeffsBasis _ x =
   let xs = map embed (coeffsCyc Pow x :: [Cyc t m r])
       bs = proxy powBasis (Proxy::Proxy m)
-  in (sum $ zipWith (*) xs bs) == x
+  in sum (zipWith (*) xs bs) == x
 
 -- verifies that CRT set elements satisfy c_i * c_j = delta_ij * c_i
 -- necessary (but not sufficient) condition
@@ -77,4 +78,4 @@ prop_crtSet_pairs :: forall (t :: Factored -> * -> *) m (m' :: Factored) (r :: *
 prop_crtSet_pairs _ =
   let crtset = proxy crtSet (Proxy::Proxy m) :: [Cyc t m' r]
       pairs = join (liftM2 (,)) crtset
-  in and $ map (\(a,b) -> if a == b then a*b == a else a*b == zero) pairs
+  in all (\(a,b) -> if a == b then a*b == a else a*b == zero) pairs
